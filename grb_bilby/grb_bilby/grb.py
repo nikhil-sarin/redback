9ee25981237776e9c2cc5e17097b7883b289ac98
Moritz Huebner <email@moritz-huebner.de>
2020-12-10 18:33:35 +1100

    Started fixing some PEP 8 issues

--- grb_bilby/grb_bilby/grb.py
+++ grb_bilby/grb_bilby/grb.py
@@ -11,16 +11,13 @@ from .analysis import find_path
 dirname = os.path.dirname(__file__)
 
 
-class SGRB:
+class SGRB(object):
     """Class for SGRB"""
+
     def __init__(self, name, path):
         """
         :param name: Telephone number of SGRB, e.g., GRB 140903A
         :param path: Path to the GRB data
-        :param time: time of GRB data (seconds)
-        :param time_err: time error in GRB data
-        :param Lum50: Luminosity of GRB in 1e50 erg/s
-        :param Lum50_err: error in luminsoty data
         """
         self.name = name
         if path == 'default':
@@ -33,9 +30,9 @@ class SGRB:
         self.Lum50_err = []
         self.photon_index = self._get_photon_index()
         self.luminosity_data = []
-        self.T90 = self._get_T90()
+        self.T90 = self._get_t90()
 
-    def load_and_truncate_data(self, truncate = True, truncate_method = 'prompt_time_error', luminosity_data=False):
+    def load_and_truncate_data(self, truncate=True, truncate_method='prompt_time_error', luminosity_data=False):
         """
         Read data of SGRB from given path and GRB telephone number.
         Truncate the data to get rid of all but the last prompt emission point
@@ -44,16 +41,15 @@ class SGRB:
         """
         if luminosity_data:
             data_file = self.path + '/GRB' + self.name + '/GRB' + self.name + '_luminosity.dat'
-            self.luminosity_data=True
+            self.luminosity_data = True
         else:
-            data_file = self.path+'/GRB' + self.name + '/GRB' + self.name + '.dat'
-            self.luminosity_data=False
+            data_file = self.path + '/GRB' + self.name + '/GRB' + self.name + '.dat'
+            self.luminosity_data = False
         data = np.loadtxt(data_file)
-        self.time = data[:, 0]      ## time (secs)
-        self.time_err = np.abs(data[:, 1:3].T) ## \Delta time (secs)
-        self.Lum50 = data[:, 3]        ## Lum (1e50 erg/s)
+        self.time = data[:, 0]  # time (secs)
+        self.time_err = np.abs(data[:, 1:3].T)  # \Delta time (secs)
+        self.Lum50 = data[:, 3]  # Lum (1e50 erg/s)
         self.Lum50_err = np.abs(data[:, 4:].T)
-
 
         if truncate:
             if truncate_method == 'prompt_time_error':
@@ -74,7 +70,8 @@ class SGRB:
 
         return None
 
-    def _get_photon_index(self):
+    @staticmethod
+    def _get_photon_index():
         short_table = os.path.join(dirname, 'tables/SGRB_table.txt')
         sgrb = pd.read_csv(short_table, header=0,
                            error_bad_lines=False, delimiter='\t', dtype='str')
@@ -83,15 +80,19 @@ class SGRB:
                            error_bad_lines=False, delimiter='\t', dtype='str')
         frames = [lgrb, sgrb]
         data = pd.concat(frames, ignore_index=True)
-        data['BAT Photon Index (15-150 keV) (PL = simple power-law, CPL = cutoff power-law)'] = data['BAT Photon Index (15-150 keV) (PL = simple power-law, CPL = cutoff power-law)'].fillna(0)
-        photon_index = data.query('GRB == @self.name')['BAT Photon Index (15-150 keV) (PL = simple power-law, CPL = cutoff power-law)']
+        data['BAT Photon Index (15-150 keV) (PL = simple power-law, CPL = cutoff power-law)'] = data[
+            'BAT Photon Index (15-150 keV) (PL = simple power-law, CPL = cutoff power-law)'].fillna(0)
+        photon_index = data.query('GRB == @self.name')[
+            'BAT Photon Index (15-150 keV) (PL = simple power-law, CPL = cutoff power-law)']
         photon_index = photon_index.values[0]
         if photon_index == 0.:
             return 0.
         else:
-            return float(photon_index.replace("PL","").replace("CPL","").replace(",","").replace("C","").replace("~",""))
+            return float(
+                photon_index.replace("PL", "").replace("CPL", "").replace(",", "").replace("C", "").replace("~", ""))
 
-    def _get_T90(self):
+    @staticmethod
+    def _get_t90():
         short_table = os.path.join(dirname, 'tables/SGRB_table.txt')
         sgrb = pd.read_csv(short_table, header=0,
                            error_bad_lines=False, delimiter='\t', dtype='str')
@@ -100,10 +101,11 @@ class SGRB:
                            error_bad_lines=False, delimiter='\t', dtype='str')
         frames = [lgrb, sgrb]
         data = pd.concat(frames, ignore_index=True)
-        #data['BAT Photon Index (15-150 keV) (PL = simple power-law, CPL = cutoff power-law)'] = data['BAT Photon Index (15-150 keV) (PL = simple power-law, CPL = cutoff power-law)'].fillna(0)
-        T90 = data.query('GRB == @self.name')['BAT T90 [sec]']
-        T90 = T90.values[0]
-        if T90 == 0.:
+        # data['BAT Photon Index (15-150 keV) (PL = simple power-law, CPL = cutoff power-law)'] = data['BAT Photon
+        # Index (15-150 keV) (PL = simple power-law, CPL = cutoff power-law)'].fillna(0)
+        t90 = data.query('GRB == @self.name')['BAT T90 [sec]']
+        t90 = t90.values[0]
+        if t90 == 0.:
             return np.nan
         else:
-            return float(T90.replace("PL","").replace("CPL","").replace(",","").replace("C","").replace("~",""))
+            return float(t90.replace("PL", "").replace("CPL", "").replace(",", "").replace("C", "").replace("~", ""))
