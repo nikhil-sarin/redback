50ecc9b69d32be954d0ca093dd0a945347f895a9
Moritz Huebner <email@moritz-huebner.de>
2021-02-17 22:24:45 +1100

    Added trigger number test

--- test/getdata_test.py
+++ test/getdata_test.py
@@ -11,5 +11,5 @@ class TestGetTriggerNumber(unittest.TestCase):
         pass
 
     def test_trigger_number(self):
-        trigger = getdata.get_trigger_number("GRB041223")
-        print(trigger)
+        trigger = getdata.get_trigger_number("041223")
+        self.assertEqual("100585", trigger)
