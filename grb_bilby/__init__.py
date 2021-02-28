e9a619f46b414bd532a126ae7051851b38e9891d
Moritz Huebner <email@moritz-huebner.de>
2021-02-28 17:25:53 +1100

    Setting up logging

--- grb_bilby/__init__.py
+++ grb_bilby/__init__.py
@@ -1,2 +1,4 @@
 from .grb_bilby import *
 from grb_bilby.grb_bilby.sampler import fit_model
+
+__version__ = "0.1"
--- grb_bilby/grb_bilby/utils.py
+++ grb_bilby/grb_bilby/utils.py
@@ -1,7 +1,10 @@
 import logging
 import os
+from pathlib import Path
 
 import matplotlib.pyplot as plt
+
+import grb_bilby
 
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'paper.mplstyle')
@@ -15,3 +18,55 @@ def find_path(path):
         return os.path.join(dirname, '../data/GRBData')
     else:
         return path
+
+
+def setup_logger(outdir='.', label=None, log_level='INFO'):
+    """ Setup logging output: call at the start of the script to use
+
+    Parameters
+    ==========
+    outdir, label: str
+        If supplied, write the logging output to outdir/label.log
+    log_level: str, optional
+        ['debug', 'info', 'warning']
+        Either a string from the list above, or an integer as specified
+        in https://docs.python.org/2/library/logging.html#logging-levels
+    """
+
+    if type(log_level) is str:
+        try:
+            level = getattr(logging, log_level.upper())
+        except AttributeError:
+            raise ValueError('log_level {} not understood'.format(log_level))
+    else:
+        level = int(log_level)
+
+    logger = logging.getLogger('grb_bilby')
+    logger.propagate = False
+    logger.setLevel(level)
+
+    if not any([type(h) == logging.StreamHandler for h in logger.handlers]):
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(name)s %(levelname)-8s: %(message)s', datefmt='%H:%M'))
+        stream_handler.setLevel(level)
+        logger.addHandler(stream_handler)
+
+    if not any([type(h) == logging.FileHandler for h in logger.handlers]):
+        if label is not None:
+            Path(outdir).mkdir(parents=True, exist_ok=True)
+            log_file = '{}/{}.log'.format(outdir, label)
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)-8s: %(message)s', datefmt='%H:%M'))
+
+            file_handler.setLevel(level)
+            logger.addHandler(file_handler)
+
+    for handler in logger.handlers:
+        handler.setLevel(level)
+
+    logger.info(f'Running grb_bilby version: {grb_bilby.__version__}')
+
+
+setup_logger(label='test')
