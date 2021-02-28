8a84feabdafb878ea3221163ad4b1d48f0877c87
Moritz Huebner <email@moritz-huebner.de>
2021-02-28 17:02:55 +1100

    Added logger

--- grb_bilby/grb_bilby/utils.py
+++ grb_bilby/grb_bilby/utils.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import matplotlib.pyplot as plt
@@ -5,6 +6,8 @@ import matplotlib.pyplot as plt
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'paper.mplstyle')
 plt.style.use(filename)
+
+logger = logging.getLogger('grb_bilby')
 
 
 def find_path(path):
