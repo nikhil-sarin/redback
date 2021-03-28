3737a0572fc8fcc8a0ae890ac33fe3db1bc068a1
Moritz Huebner <email@moritz-huebner.de>
2021-03-28 17:45:11 +1100

    Reformated files

--- examples/kilonova_example.py
+++ examples/kilonova_example.py
@@ -1,20 +1,21 @@
 import redback
 import bilby
+
 sampler = 'pymultinest'
-#lots of different models implemented, including
-#afterglow/magnetar varieties/n_dimensional_fireball/shapelets/band function/kilonova/SNe/TDE
+# lots of different models implemented, including
+# afterglow/magnetar varieties/n_dimensional_fireball/shapelets/band function/kilonova/SNe/TDE
 model = 'kilonova_nicholl21'
 
 kne = ['at2017gfo']
 path = 'KNDir'
-#gets the photometry data for AT2017gfo, the KN associated with GW170817
+# gets the photometry data for AT2017gfo, the KN associated with GW170817
 data = redback.getdata.get_open_transient_catalog_data(transient=kne, transient_type='kilonova')
-#creates a GRBDir with GRB
+# creates a GRBDir with GRB
 
-#use default priors
-priors = redback.priors(model = model, data_mode = 'photometry')
+# use default priors
+priors = redback.priors(model=model, data_mode='photometry')
 
-result = redback.fit_model(data_type = 'kilonova',name = kne, model = model, sampler = sampler,
-                           path = path, prior = priors, data_mode = 'photometry', likelihood = 'gaussian')
-#returns a Kilonova result object
-result.plot_lightcurve(random_models = 1000)
+result = redback.fit_model(data_type='kilonova', name=kne, model=model, sampler=sampler,
+                           path=path, prior=priors, data_mode='photometry', likelihood='gaussian')
+# returns a Kilonova result object
+result.plot_lightcurve(random_models=1000)
