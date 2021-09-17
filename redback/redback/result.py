d72c0dbce6ca9dbcdc1c15995a397e5a5acc7651
Moritz Huebner <email@moritz-huebner.de>
2021-09-17 14:45:37 +1000

    Fixed call to super class which was missing information gain

--- redback/redback/result.py
+++ redback/redback/result.py
@@ -26,16 +26,26 @@ class RedbackResult(Result):
     def __init__(self, label='no_label', outdir='.', sampler=None, search_parameter_keys=None,
                  fixed_parameter_keys=None, constraint_parameter_keys=None, priors=None, sampler_kwargs=None,
                  injection_parameters=None, meta_data=None, posterior=None, samples=None, nested_samples=None,
-                 log_evidence=np.nan, log_evidence_err=np.nan, log_noise_evidence=np.nan, log_bayes_factor=np.nan,
+                 log_evidence=np.nan, log_evidence_err=np.nan, information_gain=np.nan, log_noise_evidence=np.nan, log_bayes_factor=np.nan,
                  log_likelihood_evaluations=None, log_prior_evaluations=None, sampling_time=None, nburn=None,
                  num_likelihood_evaluations=None, walkers=None, max_autocorrelation_time=None, use_ratio=None,
-                 parameter_labels=None, parameter_labels_with_unit=None, gzip=False, version=None):
-        super().__init__(label, outdir, sampler, search_parameter_keys, fixed_parameter_keys, constraint_parameter_keys,
-                         priors, sampler_kwargs, injection_parameters, meta_data, posterior, samples, nested_samples,
-                         log_evidence, log_evidence_err, log_noise_evidence, log_bayes_factor,
-                         log_likelihood_evaluations, log_prior_evaluations, sampling_time, nburn,
-                         num_likelihood_evaluations, walkers, max_autocorrelation_time, use_ratio, parameter_labels,
-                         parameter_labels_with_unit, gzip, version)
+                 parameter_labels=None, parameter_labels_with_unit=None, version=None):
+
+        super(RedbackResult, self).__init__(
+            label=label, outdir=outdir, sampler=sampler,
+            search_parameter_keys=search_parameter_keys, fixed_parameter_keys=fixed_parameter_keys,
+            constraint_parameter_keys=constraint_parameter_keys, priors=priors,
+            sampler_kwargs=sampler_kwargs, injection_parameters=injection_parameters,
+            meta_data=meta_data, posterior=posterior, samples=samples,
+            nested_samples=nested_samples, log_evidence=log_evidence,
+            log_evidence_err=log_evidence_err, information_gain=information_gain,
+            log_noise_evidence=log_noise_evidence, log_bayes_factor=log_bayes_factor,
+            log_likelihood_evaluations=log_likelihood_evaluations,
+            log_prior_evaluations=log_prior_evaluations, sampling_time=sampling_time, nburn=nburn,
+            num_likelihood_evaluations=num_likelihood_evaluations, walkers=walkers,
+            max_autocorrelation_time=max_autocorrelation_time, use_ratio=use_ratio,
+            parameter_labels=parameter_labels, parameter_labels_with_unit=parameter_labels_with_unit,
+            version=version)
 
     @property
     def plot_directory_structure(self):
