32d225ce6ae19756ebfe7a4a2561c2ae3ba3755d
Moritz Huebner <email@moritz-huebner.de>
2021-09-24 15:26:14 +1000

    Re-wrote to fit with altered Transient class

--- redback/redback/transient/prompt.py
+++ redback/redback/transient/prompt.py
@@ -1,18 +1,18 @@
+import numpy as np
+
 from .transient import Transient
 
 
 data_mode = ['counts', 'tte']
 
 class PromptTimeSeries(Transient):
-    def __init__(self, name, binning, datamode = 'tte'):
+    def __init__(self, name, binning, time, time_err, y, data_mode='tte'):
+        super().__init__(time=time, time_err=time_err, y=y, y_err=np.sqrt(y), name=name, data_mode=data_mode)
         self.time_tagged_events = []
-        self.times = []
-        self.counts = []
-
-        self.name = name
 
         self._set_data()
         self._get_redshift()
+        self.binning = binning
 
     def _set_data(self):
         pass
