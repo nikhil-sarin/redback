cf9edfc76e8f5c445c7b542df71b44812c8b84e8
Moritz Huebner <email@moritz-huebner.de>
2021-10-01 15:31:04 +1000

    Modified script

--- examples/kilonova_example.py
+++ examples/kilonova_example.py
@@ -2,26 +2,27 @@ import matplotlib.pyplot as plt
 
 import redback
 import bilby
+
 sampler = 'pymultinest'
-#lots of different models implemented, including
-#afterglow/magnetar varieties/n_dimensional_fireball/shapelets/band function/kilonova/SNe/TDE
+# lots of different models implemented, including
+# afterglow/magnetar varieties/n_dimensional_fireball/shapelets/band function/kilonova/SNe/TDE
 model = 'kilonova_nicholl21'
 
 kne = 'at2017gfo'
 path = 'KNDir'
-#gets the photometry data for AT2017gfo, the KN associated with GW170817
-data = redback.getdata.get_open_transient_catalog_data(transient=kne, transient_type='kilonova', use_default_directory=False)
-#creates a GRBDir with GRB
+# gets the photometry data for AT2017gfo, the KN associated with GW170817
+data = redback.getdata.get_open_transient_catalog_data(transient=kne, transient_type='kilonova',
+                                                       use_default_directory=False)
+# creates a GRBDir with GRB
 kilonova = redback.kilonova.Kilonova.from_open_access_catalogue(transient=kne, data_mode="photometry")
 fig, axes = plt.subplots(3, 2, sharex=True, sharey=True, figsize=(12, 8))
 kilonova.flux_density_data = True
 kilonova.plot_multiband(figure=fig, axes=axes, filters=["g", "r", "i", "z", "y", "J"])
 
-assert False
-#use default priors
-priors = redback.priors(model = model, data_mode = 'photometry')
+# use default priors
+priors = redback.priors.get_priors(model=model, data_mode='photometry')
 
-result = redback.fit_model(data_type = 'kilonova',name = kne, model = model, sampler = sampler,
-                           path = path, prior = priors, data_mode = 'photometry', likelihood = 'gaussian')
-#returns a Kilonova result object
-result.plot_lightcurve(random_models = 1000)
+result = redback.fit_model(data_type='kilonova', name=kne, model=model, sampler=sampler,
+                           path=path, prior=priors, data_mode='photometry', likelihood='gaussian')
+# returns a Kilonova result object
+result.plot_lightcurve(random_models=1000)
