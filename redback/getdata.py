dc9ffb43a3fd9d1c640e9aac8cf7dc765bc1274d
Moritz Huebner <email@moritz-huebner.de>
2021-10-07 13:54:51 +1100

    Added checks to catch all get_data function

--- redback/getdata.py
+++ redback/getdata.py
@@ -26,11 +26,17 @@ dirname = os.path.dirname(__file__)
 
 SWIFT_PROMPT_BIN_SIZES = ['1s', '2ms', '8ms', '16ms', '64ms', '256ms']
 
-data_source = ["swift", "swift_xrt", "fermi", "konus", "batse", "open_data"]
-transient_types = ["afterglow", "prompt", "xrt"]
+DATA_SOURCES = ["swift", "swift_xrt", "fermi", "konus", "batse", "open_data"]
+TRANSIENT_TYPES = ["afterglow", "prompt", "kilonova", "supernova", "tidal_disruption_event"]
 
 
 def get_data(transient_type, data_source, event_label, use_default_directory, data_mode=None, **kwargs):
+    if transient_type not in TRANSIENT_TYPES:
+        raise ValueError(f"Transient type {transient_type} not allowed, "
+                         f"must be one of the following: {TRANSIENT_TYPES}")
+    if data_source not in DATA_SOURCES:
+        raise ValueError(f"Data source {data_source} not allowed, "
+                         f"must be one of the following: {DATA_SOURCES}")
     kwargs["bin_size"] = kwargs.get("bin_size", "2ms")
     func_dict = _get_data_functions_dict()
     try:
