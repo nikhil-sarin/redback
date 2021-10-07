2b87e031281d88de7cbe1e8d3efec6fd7b86182f
Moritz Huebner <email@moritz-huebner.de>
2021-10-07 15:17:43 +1100

    Added priors to prompt models

--- redback/priors.py
+++ redback/priors.py
@@ -1,11 +1,17 @@
+import numpy as np
 import os
 
+import bilby.core.prior
 from bilby.core.prior import PriorDict
 
+import redback.model_library
 from redback.utils import logger
 
 
-def get_priors(model, data_mode):
+def get_priors(model, data_mode, times=None, y=None, yerr=None):
+    if model in redback.model_library.models_dict['prompt_models']:
+        return get_prompt_priors()
+
     priors = PriorDict()
     try:
         filename = os.path.join(os.path.dirname(__file__), 'priors', f'{model}.prior')
@@ -14,3 +20,55 @@ def get_priors(model, data_mode):
         logger.warning(e)
         logger.warning('Returning empty PriorDict.')
     return priors
+
+
+def get_prompt_priors(model, times, y, yerr, **kwargs):
+    if model == 'gaussian':
+        get_gaussian_priors(times=times, y=y, yerr=yerr, **kwargs)
+
+
+def get_gaussian_priors(times, y, yerr, **kwargs):
+    dt = np.min(np.diff(times))
+    duration = times[-1] - times[0]
+    priors = bilby.core.prior.PriorDict()
+    priors['amplitude'] = bilby.core.prior.LogUniform(minimum=np.min(yerr), maximum=np.max(y),
+                                                      name='amplitude', latex_label=r'$A$')
+    priors['sigma'] = bilby.core.prior.LogUniform(minimum=3*dt, maximum=duration, name="sigma", latex_label=r"$\sigma$")
+    priors['t_0'] = bilby.core.prior.Uniform(minimum=times[0], maximum=times[-1], name="t_0", latex_label=r"$t_0$")
+    return priors
+
+
+def get_skew_gaussian_priors(times, y, yerr, **kwargs):
+    priors = get_gaussian_priors(times=times, y=y, yerr=yerr, **kwargs)
+    for latex_label, part in zip([r"$\sigma_{\mathrm{rise}}$" r"$\sigma_{\mathrm{rise}}$"], ['rise', 'fall']):
+        priors[f'sigma_{part}'] = bilby.core.prior.LogUniform(
+            minimum=priors['sigma'].minimum, maximum=priors['sigma'].maximum,
+            name=f"sigma_{part}", latex_label=latex_label)
+    del priors['sigma']
+    return priors
+
+
+def get_skew_exponential_priors(times, y, yerr, **kwargs):
+    priors = get_gaussian_priors(times=times, y=y, yerr=yerr, **kwargs)
+    for latex_label, part in zip([r"$\tau_{\mathrm{rise}}$" r"$\tau_{\mathrm{rise}}$"], ['rise', 'fall']):
+        priors[f'tau_{part}'] = bilby.core.prior.LogUniform(
+            minimum=priors['sigma'].minimum, maximum=priors['sigma'].maximum,
+            name=f"tau_{part}", latex_label=latex_label)
+    del priors['sigma']
+    return priors
+
+
+def get_fred_priors(times, y, yerr, **kwargs):
+    priors = bilby.core.prior.PriorDict()
+    priors['amplitude'] = bilby.core.prior.LogUniform(minimum=np.min(yerr), maximum=np.max(y),
+                                                      name='amplitude', latex_label=r'$A$')
+    priors['tau'] = bilby.core.prior.Uniform(minimum=1e-3, maximum=1e3, name="t_0", latex_label=r"$t_0$")
+    priors['psi'] = bilby.core.prior.Uniform(minimum=1e-3, maximum=1e3, name=r"\psi")
+    priors['delta'] = bilby.core.prior.Uniform(minimum=times[0], maximum=times[-1], name=r"\delta")
+    return priors
+
+
+def get_fred_extended_priors(times, y, yerr, **kwargs):
+    priors = get_fred_priors(times=times, y=y, yerr=yerr, **kwargs)
+    priors['gamma'] = bilby.core.prior.LogUniform(minimum=1e-3, maximum=1e3, name=r"$\gamma$")
+    priors['nu'] = bilby.core.prior.LogUniform(minimum=1e-3, maximum=1e3, name=r"$\nu")
--- redback/transient_models/prompt_models.py
+++ redback/transient_models/prompt_models.py
@@ -1,56 +1,43 @@
 import numpy as np
 
 
-def skew_exponential(times, log_amplitude, t_0, log_sigma_rise, log_sigma_fall, **kwargs):
+def gaussian(times, amplitude, t_0, sigma, **kwargs):
     dt = kwargs.get('dt', 1)
-    amplitude = np.exp(log_amplitude)
-    sigma_rise = np.exp(log_sigma_rise)
-    sigma_fall = np.exp(log_sigma_fall)
-
-    before_burst_indices = np.where(times <= t_0)
-    after_burst_indices = np.where(times > t_0)
-    envelope = np.zeros(len(times))
-    envelope[before_burst_indices] = amplitude * np.exp((times[before_burst_indices] - t_0) / sigma_rise)
-    envelope[after_burst_indices] = amplitude * np.exp(-(times[after_burst_indices] - t_0) / sigma_fall)
-    return envelope * dt
-
-
-def gaussian(times, log_amplitude, t_0, log_sigma, **kwargs):
-    dt = kwargs.get('dt', 1)
-    amplitude = np.exp(log_amplitude)
-    sigma = np.exp(log_sigma)
     return amplitude * np.exp(-(times - t_0) ** 2 / (2 * sigma ** 2)) * dt
 
 
-def skew_gaussian(times, log_amplitude, t_0, log_sigma_rise, log_sigma_fall, **kwargs):
+def skew_gaussian(times, amplitude, t_0, sigma_rise, sigma_fall, **kwargs):
     dt = kwargs.get('dt', 1)
     before_burst_indices = np.where(times <= t_0)
     after_burst_indices = np.where(times > t_0)
     envelope = np.zeros(len(times))
-    envelope[before_burst_indices] = gaussian(times=times[before_burst_indices], log_amplitude=log_amplitude,
-                                              t_0=t_0, log_sigma=log_sigma_rise)
-    envelope[after_burst_indices] = gaussian(times=times[after_burst_indices], log_amplitude=log_amplitude,
-                                             t_0=t_0, log_sigma=log_sigma_fall)
+    envelope[before_burst_indices] = gaussian(
+        times=times[before_burst_indices], amplitude=amplitude,t_0=t_0, sigma=sigma_rise)
+    envelope[after_burst_indices] = gaussian(
+        times=times[after_burst_indices], amplitude=amplitude, t_0=t_0, sigma=sigma_fall)
     return envelope * dt
 
 
-def fred(times, log_amplitude, log_psi, t_0, delta, **kwargs):
+def skew_exponential(times, amplitude, t_0, tau_rise, tau_fall, **kwargs):
     dt = kwargs.get('dt', 1)
-    amplitude = np.exp(log_amplitude)
-    psi = np.exp(log_psi)
 
-    frac = (times + delta) / t_0
+    before_burst_indices = np.where(times <= t_0)
+    after_burst_indices = np.where(times > t_0)
+    envelope = np.zeros(len(times))
+    envelope[before_burst_indices] = amplitude * np.exp((times[before_burst_indices] - t_0) / tau_rise)
+    envelope[after_burst_indices] = amplitude * np.exp(-(times[after_burst_indices] - t_0) / tau_fall)
+    return envelope * dt
+
+
+def fred(times, amplitude, psi, tau, delta, **kwargs):
+    dt = kwargs.get('dt', 1)
+    frac = (times - delta) / tau
     with np.errstate(divide='ignore', invalid='ignore', over='ignore'):
         return amplitude * np.exp(-psi * (frac + 1 / frac)) * np.exp(2 * psi) * dt
 
 
-def fred_extended(times, log_amplitude, log_psi, t_0, delta, log_gamma, log_nu, **kwargs):
+def fred_extended(times, amplitude, psi, tau, delta, gamma, nu, **kwargs):
     dt = kwargs.get('dt', 1)
-    amplitude = np.exp(log_amplitude)
-    nu = np.exp(log_nu)
-    gamma = np.exp(log_gamma)
-    psi = np.exp(log_psi)
-
-    frac = (times + delta) / t_0
+    frac = (times - delta) / tau
     with np.errstate(divide='ignore', invalid='ignore', over='ignore'):
         return amplitude * np.exp(-psi ** gamma * frac ** gamma - psi ** nu / frac ** nu) * np.exp(2 * psi) * dt
