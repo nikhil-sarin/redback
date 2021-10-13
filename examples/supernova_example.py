0f2fff980315a2a364366e6bf321aecbc0b231ce
Moritz Huebner <email@moritz-huebner.de>
2021-10-13 17:08:52 +1100

    Added example stumps

---
+++ examples/supernova_example.py
@@ -0,0 +1,4 @@
+import redback
+
+
+redback.getdata.get_supernova_data_from_open_transient_catalog_data("SN2003dh")
---
+++ examples/tde_example.py
@@ -0,0 +1,4 @@
+import redback
+
+
+redback.getdata.get_tidal_disruption_event_data_from_open_transient_catalog_data("PS18kh")
