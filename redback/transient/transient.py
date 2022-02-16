506b95a2f6b569bd5dcce75162f5b0dd68022dfd
Moritz Huebner <email@moritz-huebner.de>
2022-02-16 22:53:07 +1100

    Added type hints and constructor documentation stubs

--- redback/transient/transient.py
+++ redback/transient/transient.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 from os.path import join
 import pandas as pd
+from typing import Union
 
 import redback
 
@@ -25,13 +28,48 @@ class Transient(object):
     counts_data = redback.utils.DataModeSwitch('counts')
     tte_data = redback.utils.DataModeSwitch('ttes')
 
-    def __init__(self, time=None, time_err=None, time_mjd=None, time_mjd_err=None, time_rest_frame=None,
-                 time_rest_frame_err=None, Lum50=None, Lum50_err=None, flux=None, flux_err=None, flux_density=None,
-                 flux_density_err=None, magnitude=None, magnitude_err=None, counts=None, ttes=None, bin_size=None,
-                 redshift=np.nan, data_mode=None, name='', path='.', photon_index=np.nan, use_phase_model=False,
-                 frequency=None, system=None, bands=None, active_bands=None, **kwargs):
+    def __init__(
+            self, time: np.ndarray = None, time_err: np.ndarray = None, time_mjd: np.ndarray = None,
+            time_mjd_err: np.ndarray = None, time_rest_frame: np.ndarray = None, time_rest_frame_err: np.ndarray = None,
+            Lum50: np.ndarray = None, Lum50_err: np.ndarray = None, flux: np.ndarray = None,
+            flux_err: np.ndarray = None, flux_density: np.ndarray = None, flux_density_err: np.ndarray = None,
+            magnitude: np.ndarray = None, magnitude_err: np.ndarray = None, counts: np.ndarray = None,
+            ttes: np.ndarray = None, bin_size: float = None, redshift: float = np.nan, data_mode: str = None,
+            name: str = '', path: str = '.', photon_index: float = np.nan, use_phase_model: bool = False,
+            frequency: np.ndarray = None, system: np.ndarray = None, bands: np.ndarray = None,
+            active_bands: Union[np.ndarray, str] = None, **kwargs: dict) -> None:
         """
-        Base class for all transients
+
+        Parameters
+        ----------
+        time: array_like
+        time_err: array_like
+        time_mjd: array_like
+        time_mjd_err: array_like
+        time_rest_frame: array_like
+        time_rest_frame_err: array_like
+        Lum50: array_like
+        Lum50_err: array_like
+        flux: array_like
+        flux_err: array_like
+        flux_density: array_like
+        flux_density_err: array_like
+        magnitude: array_like
+        magnitude_err: array_like
+        counts: array_like
+        ttes: array_like
+        bin_size: float
+        redshift: float
+        data_mode: str
+        name: str
+        path: str
+        photon_index: float
+        use_phase_model: bool
+        frequency: array_like
+        system: array_like
+        bands: list
+        active_bands: list
+        kwargs: callable
         """
         self.bin_size = bin_size
         self.bin_ttes = kwargs.get("bin_ttes", redback.utils.bin_ttes)
@@ -74,7 +112,7 @@ class Transient(object):
         self.photon_index = photon_index
 
     @property
-    def _time_attribute_name(self):
+    def _time_attribute_name(self) -> str:
         if self.luminosity_data:
             return "time_rest_frame"
         elif self.use_phase_model:
@@ -82,97 +120,97 @@ class Transient(object):
         return "time"
 
     @property
-    def _time_err_attribute_name(self):
+    def _time_err_attribute_name(self) -> str:
         return self._time_attribute_name + "_err"
 
     @property
-    def _y_attribute_name(self):
+    def _y_attribute_name(self) -> str:
         return self._ATTRIBUTE_NAME_DICT[self.data_mode]
 
     @property
-    def _y_err_attribute_name(self):
+    def _y_err_attribute_name(self) -> str:
         return self._ATTRIBUTE_NAME_DICT[self.data_mode] + "_err"
 
     @property
-    def x(self):
+    def x(self) -> np.ndarray:
         return getattr(self, self._time_attribute_name)
 
     @x.setter
-    def x(self, x):
+    def x(self, x: np.ndarray) -> None:
         setattr(self, self._time_attribute_name, x)
 
     @property
-    def x_err(self):
+    def x_err(self) -> np.ndarray:
         return getattr(self, self._time_err_attribute_name)
 
     @x_err.setter
-    def x_err(self, x_err):
+    def x_err(self, x_err: np.ndarray) -> None:
         setattr(self, self._time_err_attribute_name, x_err)
 
     @property
-    def y(self):
+    def y(self) -> np.ndarray:
         return getattr(self, self._y_attribute_name)
 
     @y.setter
-    def y(self, y):
+    def y(self, y: np.ndarray) -> None:
         setattr(self, self._y_attribute_name, y)
 
     @property
-    def y_err(self):
+    def y_err(self) -> np.ndarray:
         return getattr(self, self._y_err_attribute_name)
 
     @y_err.setter
-    def y_err(self, y_err):
+    def y_err(self, y_err: np.ndarray) -> None:
         setattr(self, self._y_err_attribute_name, y_err)
 
     @property
-    def data_mode(self):
+    def data_mode(self) -> str:
         return self._data_mode
 
     @data_mode.setter
-    def data_mode(self, data_mode):
+    def data_mode(self, data_mode: str) -> None:
         if data_mode in self.DATA_MODES or data_mode is None:
             self._data_mode = data_mode
         else:
             raise ValueError("Unknown data mode.")
 
     @property
-    def xlabel(self):
+    def xlabel(self) -> str:
         if self.use_phase_model:
             return r"Time [MJD]"
         else:
             return r"Time since burst [days]"
 
     @property
-    def ylabel(self):
+    def ylabel(self) -> str:
         try:
             return self.ylabel_dict[self.data_mode]
         except KeyError:
             raise ValueError("No data mode specified")
 
     @property
-    def frequency(self):
+    def frequency(self) -> np.ndarray:
         return self._frequency
 
     @frequency.setter
-    def frequency(self, frequency):
+    def frequency(self, frequency: np.ndarray) -> None:
         if frequency is None:
             self._frequency = self.bands_to_frequencies(self.bands)
         else:
             self._frequency = frequency
 
     @property
-    def active_bands(self):
+    def active_bands(self) -> np.ndarray:
         return self._active_bands
 
     @active_bands.setter
-    def active_bands(self, active_bands):
+    def active_bands(self, active_bands: np.ndarray) -> None:
         if active_bands == 'all':
             self._active_bands = np.unique(self.bands)
         else:
             self._active_bands = active_bands
 
-    def get_filtered_data(self):
+    def get_filtered_data(self) -> tuple:
         if self.flux_density_data or self.photometry_data:
             idxs = [b in self.active_bands for b in self.bands]
             filtered_x = self.x[idxs]
@@ -188,35 +226,37 @@ class Transient(object):
                              f"but is in {self.data_mode} instead.")
 
     @property
-    def unique_bands(self):
+    def unique_bands(self) -> np.ndarray:
         return np.unique(self.bands)
 
     @property
-    def unique_frequencies(self):
+    def unique_frequencies(self) -> np.ndarray:
         return self.bands_to_frequencies(self.unique_bands)
 
     @property
-    def list_of_band_indices(self):
+    def list_of_band_indices(self) -> list:
         return [np.where(self.bands == np.array(b))[0] for b in self.unique_bands]
 
     @property
-    def default_filters(self):
+    def default_filters(self) -> list:
         return ["g", "r", "i", "z", "y", "J", "H", "K"]
 
     @staticmethod
-    def get_colors(filters):
+    def get_colors(filters: Union[np.ndarray, list]) -> matplotlib.colors.Colormap:
         return matplotlib.cm.rainbow(np.linspace(0, 1, len(filters)))
 
-    def plot_data(self, axes=None, colour='k'):
+    def plot_data(self, axes: matplotlib.axes.Axes = None, colour: str = 'k') -> matplotlib.axes.Axes:
         fig, axes = plt.subplots()
         return axes
 
-    def plot_multiband(self, axes=None, colour='k'):
+    def plot_multiband(self, axes: matplotlib.axes.Axes = None, colour: str = 'k') -> matplotlib.axes.Axes:
         fig, axes = plt.subplots()
         return axes
 
-    def plot_lightcurve(self, model, filename=None, axes=None, plot_save=True, plot_show=True, random_models=100,
-                        posterior=None, outdir='.', model_kwargs=None, **kwargs):
+    def plot_lightcurve(
+            self, model: callable, filename: str = None, axes: matplotlib.axes.Axes = None,  plot_save: bool = True, 
+            plot_show: bool = True, random_models: int = 100, posterior: pd.DataFrame = None, outdir: str = '.', 
+            model_kwargs: dict = None, **kwargs: object) -> None:
         if filename is None:
             filename = f"{self.name}_lightcurve.png"
         axes = axes or plt.gca()
@@ -236,8 +276,10 @@ class Transient(object):
         plt.savefig(join(outdir, filename), dpi=300, bbox_inches="tight")
         plt.clf()
 
-    def plot_multiband_lightcurve(self, model, filename=None, axes=None, plot_save=True, plot_show=True,
-                                  random_models=100, posterior=None, outdir='.', model_kwargs=None, **kwargs):
+    def plot_multiband_lightcurve(
+            self, model: callable, filename: str = None, axes: matplotlib.axes.Axes = None, plot_save: bool = True, 
+            plot_show: bool = True, random_models: int = 100, posterior: pd.DataFrame = None, outdir: str = '.', 
+            model_kwargs: dict = None, **kwargs: object) -> None:
         if self.luminosity_data or self.flux_data:
             redback.utils.logger.warning(f"Plotting multiband lightcurve not possible for {self.data_mode}. Returning.")
             return
@@ -267,7 +309,7 @@ class Transient(object):
             plt.show()
         plt.clf()
 
-    def _get_times(self, axes):
+    def _get_times(self, axes: matplotlib.axes.Axes) -> np.ndarray:
         if axes.get_yscale == 'linear':
             times = np.linspace(self.x[0], self.x[-1], 200)
         else:
@@ -277,21 +319,6 @@ class Transient(object):
 
 class OpticalTransient(Transient):
     DATA_MODES = ['flux', 'flux_density', 'photometry', 'luminosity']
-
-    def __init__(self, name, data_mode='photometry', time=None, time_err=None, time_mjd=None, time_mjd_err=None,
-                 time_rest_frame=None, time_rest_frame_err=None, Lum50=None, Lum50_err=None, flux_density=None,
-                 flux_density_err=None, magnitude=None, magnitude_err=None, frequency=None, bands=None, system=None,
-                 active_bands='all',
-                 use_phase_model=False, **kwargs):
-
-        super().__init__(time=time, time_err=time_err, time_rest_frame=time_rest_frame, time_mjd=time_mjd,
-                         time_mjd_err=time_mjd_err, frequency=frequency,
-                         time_rest_frame_err=time_rest_frame_err, Lum50=Lum50, Lum50_err=Lum50_err,
-                         flux_density=flux_density, flux_density_err=flux_density_err, magnitude=magnitude,
-                         magnitude_err=magnitude_err, data_mode=data_mode, name=name,
-                         use_phase_model=use_phase_model, system=system, bands=bands, active_bands=active_bands,
-                         **kwargs)
-        self._set_data()
 
     @staticmethod
     def load_data(name, data_mode='photometry', transient_dir="."):
@@ -314,8 +341,52 @@ class OpticalTransient(Transient):
         elif data_mode == "all":
             return time_days, time_mjd, flux_density, flux_density_err, magnitude, magnitude_err, bands, system
 
+    def __init__(
+            self, name: str, data_mode: str = 'photometry', time: np.ndarray = None, time_err: np.ndarray = None, 
+            time_mjd: np.ndarray = None, time_mjd_err: np.ndarray = None, time_rest_frame: np.ndarray = None, 
+            time_rest_frame_err: np.ndarray = None, Lum50: np.ndarray = None, Lum50_err: np.ndarray = None,
+            flux_density: np.ndarray = None, flux_density_err: np.ndarray = None, magnitude: np.ndarray = None, 
+            magnitude_err: np.ndarray = None, frequency: np.ndarray = None, bands: np.ndarray = None, 
+            system: np.ndarray = None, active_bands: Union[np.ndarray, str] = 'all', use_phase_model: bool = False, 
+            **kwargs: dict) -> None:
+        """
+
+        Parameters
+        ----------
+        name
+        data_mode
+        time
+        time_err
+        time_mjd
+        time_mjd_err
+        time_rest_frame
+        time_rest_frame_err
+        Lum50
+        Lum50_err
+        flux_density
+        flux_density_err
+        magnitude
+        magnitude_err
+        frequency
+        bands
+        system
+        active_bands
+        use_phase_model
+        kwargs
+        """
+        super().__init__(time=time, time_err=time_err, time_rest_frame=time_rest_frame, time_mjd=time_mjd,
+                         time_mjd_err=time_mjd_err, frequency=frequency,
+                         time_rest_frame_err=time_rest_frame_err, Lum50=Lum50, Lum50_err=Lum50_err,
+                         flux_density=flux_density, flux_density_err=flux_density_err, magnitude=magnitude,
+                         magnitude_err=magnitude_err, data_mode=data_mode, name=name,
+                         use_phase_model=use_phase_model, system=system, bands=bands, active_bands=active_bands,
+                         **kwargs)
+        self._set_data()
+
     @classmethod
-    def from_open_access_catalogue(cls, name, data_mode="photometry", active_bands='all', use_phase_model=False):
+    def from_open_access_catalogue(
+            cls, name: str, data_mode: str = "photometry", active_bands: Union[np.ndarray, str] = 'all',
+            use_phase_model: bool = False) -> OpticalTransient:
         transient_dir = cls._get_transient_dir(name=name)
         time_days, time_mjd, flux_density, flux_density_err, magnitude, magnitude_err, bands, system = \
             cls.load_data(name=name, transient_dir=transient_dir, data_mode="all")
@@ -325,10 +396,10 @@ class OpticalTransient(Transient):
                    use_phase_model=use_phase_model)
 
     @property
-    def event_table(self):
+    def event_table(self) -> str:
         return f'{self.__class__.__name__.lower()}/{self.name}/metadata.csv'
 
-    def _set_data(self):
+    def _set_data(self) -> None:
         try:
             meta_data = pd.read_csv(self.event_table, error_bad_lines=False, delimiter=',', dtype='str')
         except FileNotFoundError as e:
@@ -338,16 +409,18 @@ class OpticalTransient(Transient):
         self.meta_data = meta_data
 
     @property
-    def transient_dir(self):
+    def transient_dir(self) -> str:
         return self._get_transient_dir(name=self.name)
 
     @classmethod
-    def _get_transient_dir(cls, name):
+    def _get_transient_dir(cls, name: str) -> str:
         transient_dir, _, _ = redback.getdata.transient_directory_structure(
             transient=name, transient_type=cls.__name__.lower())
         return transient_dir
 
-    def plot_data(self, axes=None, filters=None, plot_others=True, **plot_kwargs):
+    def plot_data(
+            self, axes: matplotlib.axes.Axes = None, filters: np.ndarray = None, plot_others: bool = True, 
+            **plot_kwargs: dict) -> None:
         """
         plots the data
         :param axes:
@@ -395,8 +468,10 @@ class OpticalTransient(Transient):
         plt.savefig(join(self.transient_dir, filename))
         plt.clf()
 
-    def plot_multiband(self, figure=None, axes=None, ncols=2, nrows=None, figsize=None, filters=None,
-                       **plot_kwargs):
+    def plot_multiband(
+            self, figure: matplotlib.figure.Figure = None, axes: matplotlib.axes.Axes = None, ncols: int = 2,
+            nrows: int = None, figsize: tuple = None, filters: np.ndarray = None, **plot_kwargs: dict) \
+            -> matplotlib.axes.Axes:
         if self.luminosity_data or self.flux_data:
             redback.utils.logger.warning(f"Can't plot multiband for {self.data_mode} data.")
             return
