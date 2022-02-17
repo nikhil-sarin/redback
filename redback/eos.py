bcc2d283c97f0f806c7572d53e1c138495ffffaf
Nikhil Sarin <nikhil.sarin@monash.edu>
2022-02-17 10:17:52 +0100

    add the lambda calculation to the EOS class

--- redback/eos.py
+++ redback/eos.py
@@ -68,7 +68,7 @@ class Piecewise_polytrope(object):
         return np.array(r_tmp)
 
 
-    def Lambda_of_central_pressure(self, central_pressure):
+    def lambda_of_central_pressure(self, central_pressure):
         """
         :param central_pressure:
         :return: dimensionless tidal deformability
@@ -80,53 +80,72 @@ class Piecewise_polytrope(object):
         Lambda = (2 / 3) * k2 * (cc.c.si.value ** 2 * radius / (cc.G.si.value * mass)) ** 5
         return Lambda
 
-    def Lambda_of_mass(self, central_pressure, mass, maximum_mass_limit=1.97):
+    def lambda_array_of_central_pressure(self, central_pressure_array, maximum_mass_lower_limit=2.01):
+        """
+        :param central_pressure_array: array of central pressure in SI units
+        :param maximum_mass_lower_limit: 2.01 solar masses, Throw out EOS's that are below this value.
+        Users should enforce this at the prior level.
+        :return: dimensionless tidal deformability
+        """
+
+        tmp = np.array([lambda_of_central_pressure(pp) for pp in central_pressure_array])
+        mass = tmp[:, 0]
+        lambdas = tmp[:, 1]
+
+        arg_maximum_mass = np.argmax(mass)
+        max_mass = mass[arg_maximum_mass]
+
+        if max_mass < maximum_mass_lower_limit:
+            raise ValueError("Maximum mass for this EOS is lower than 2.01, please choose a more realistic EOS")
+
+        # Choose masses between 1. and maximum mass
+        args = np.argwhere((mass >= 1.)).flatten()
+        mass = mass[args[0]:arg_maximum_mass]
+        lambdas = lambdas[args[0]:arg_maximum_mass]
+
+        return mass, lambdas, max_mass
+
+    def lambda_of_mass(self, central_pressure, mass, maximum_mass_lower_limit=2.01):
         """
         :param central_pressure: central pressure in SI units
         :param mass: neutron star masses in solar masses
         :param maximum_mass_limit:
         :return: lambda for the given mass array
         """
-        mass_tmp, Lambda_tmp, max_mass = self.Lambda_array_of_central_pressure(central_pressure, maximum_mass_limit)
+        mass_tmp, Lambda_tmp, max_mass = self.lambda_array_of_central_pressure(central_pressure, maximum_mass_lower_limit)
 
-        if max_mass >= maximum_mass_limit:
-            if hasattr(mass, '__len__'):
-                if mass.shape == (len(mass),):  # i.e. np.array([mass_1])
-                    interpolated_Lambda = interp1d(
-                        mass_tmp, Lambda_tmp, fill_value='extrapolate')
-
-                    Lambda = interpolated_Lambda(mass)
-                    args = np.argwhere(Lambda >= 0).flatten()
-                    args_2 = np.argwhere(Lambda < 0).flatten()
-
-                    # We append zeros after a NS collapses, i.e, for masses > M_tov
-                    Lambda = np.append(Lambda[args], np.zeros(len(args_2)))
-
-                elif mass.shape == (2, len(mass[0])):  # i.e. np.array([mass_1,mass_2])
-                    interpolated_Lambda = interp1d(
-                        mass_tmp, Lambda_tmp, fill_value='extrapolate')
-                    Lambda = np.zeros([2, len(mass[0])])
-                    for ii in range(2):
-                        Lambda_tmp_2 = interpolated_Lambda(mass[ii])
-                        args = np.argwhere(Lambda_tmp_2 >= 0).flatten()
-                        args_2 = np.argwhere(Lambda_tmp_2 < 0).flatten()
-
-                        # We append zeros after a NS collapses, i.e, for masses > M_tov
-                        Lambda[ii] = np.append(Lambda_tmp_2[args], np.zeros(len(args_2)))
-
-            else:
-                # if mass is a float
+        if hasattr(mass, '__len__'):
+            if mass.shape == (len(mass),):  # i.e. np.array([mass_1])
                 interpolated_Lambda = interp1d(
                     mass_tmp, Lambda_tmp, fill_value='extrapolate')
+
                 Lambda = interpolated_Lambda(mass)
-                if Lambda < 0:
-                    Lambda = 0
+                args = np.argwhere(Lambda >= 0).flatten()
+                args_2 = np.argwhere(Lambda < 0).flatten()
+
+                # We append zeros after a NS collapses, i.e, for masses > M_tov
+                Lambda = np.append(Lambda[args], np.zeros(len(args_2)))
+
+            elif mass.shape == (2, len(mass[0])):  # i.e. np.array([mass_1,mass_2])
+                interpolated_Lambda = interp1d(
+                    mass_tmp, Lambda_tmp, fill_value='extrapolate')
+                Lambda = np.zeros([2, len(mass[0])])
+                for ii in range(2):
+                    Lambda_tmp_2 = interpolated_Lambda(mass[ii])
+                    args = np.argwhere(Lambda_tmp_2 >= 0).flatten()
+                    args_2 = np.argwhere(Lambda_tmp_2 < 0).flatten()
+
+                    # We append zeros after a NS collapses, i.e, for masses > M_tov
+                    Lambda[ii] = np.append(Lambda_tmp_2[args], np.zeros(len(args_2)))
 
         else:
-            # if the maximum_mass is below the maximum mass supported by
-            # pulsar observations, we set Lambda=np.nan. These values
-            # should be ruled out by the prior
-            Lambda = np.nan
+            # if mass is a float
+            interpolated_Lambda = interp1d(
+                mass_tmp, Lambda_tmp, fill_value='extrapolate')
+            Lambda = interpolated_Lambda(mass)
+            if Lambda < 0:
+                Lambda = 0
+
 
         return Lambda, max_mass
 
