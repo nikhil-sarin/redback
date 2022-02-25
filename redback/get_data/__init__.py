15c2297174f69a9c4ee6e958173e8d890cbc099d
Moritz Huebner <email@moritz-huebner.de>
2022-02-26 00:59:19 +1100

    Added type hints

--- redback/get_data/__init__.py
+++ redback/get_data/__init__.py
@@ -1,24 +1,27 @@
+from __future__ import annotations
+
 import redback.get_data.directory
 import redback.get_data.open_data
 import redback.get_data.swift
 import redback.get_data.utils
+from redback.get_data.swift import SwiftDataGetter
 
 
-def get_xrt_data_from_swift(grb: str, data_mode: str = None, **kwargs: dict) -> redback.get_data.swift.SwiftDataGetter:
+def get_xrt_data_from_swift(grb: str, data_mode: str = None, **kwargs: dict) -> SwiftDataGetter:
     return get_swift_data(grb=grb, transient_type='afterglow', data_mode=data_mode, instrument="XRT")
 
 
-def get_afterglow_data_from_swift(grb: str, data_mode: str, **kwargs: dict) -> redback.get_data.swift.SwiftDataGetter:
+def get_afterglow_data_from_swift(grb: str, data_mode: str, **kwargs: dict) -> SwiftDataGetter:
     return get_swift_data(grb=grb, transient_type='afterglow', data_mode=data_mode, instrument="BAT+XRT")
 
 
-def get_prompt_data_from_swift(grb: str, bin_size: str = "1s", **kwargs: dict) -> redback.get_data.swift.SwiftDataGetter:
+def get_prompt_data_from_swift(grb: str, bin_size: str = "1s", **kwargs: dict) -> SwiftDataGetter:
     return get_swift_data(grb=grb, transient_type='prompt', data_mode='prompt', instrument="BAT+XRT", bin_size=bin_size)
 
 
 def get_swift_data(
         grb: str, transient_type: str, data_mode: str = 'flux', instrument: str = 'BAT+XRT',
-        bin_size: str = None) -> redback.get_data.swift.SwiftDataGetter:
+        bin_size: str = None) -> SwiftDataGetter:
     getter = swift.SwiftDataGetter(
         grb=grb, transient_type=transient_type, data_mode=data_mode,
         bin_size=bin_size, instrument=instrument)
@@ -29,7 +32,7 @@ def get_swift_data(
 _functions_dict = {
             ("afterglow", "swift"): get_afterglow_data_from_swift,
             ("afterglow", "swift_xrt"): get_xrt_data_from_swift,
-            ("prompt", "swift"): get_prompt_data_from_swift,}
+            ("prompt", "swift"): get_prompt_data_from_swift}
             # ("prompt", "fermi"): get_prompt_data_from_fermi,
             # ("prompt", "konus"): get_prompt_data_from_konus,
             # ("prompt", "batse"): get_prompt_data_from_batse,
