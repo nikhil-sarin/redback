4716271eac3a0fe5244d7f4ce6842b3ff1922d11
Nikhil Sarin <nikhil.sarin@monash.edu>
2022-02-25 14:26:31 +0100

    Some new constants

--- redback/constants.py
+++ redback/constants.py
@@ -1,4 +1,5 @@
 import astropy.constants as cc
+import astropy.units as uu
 
 ev_to_erg = 1.60218e-12
 speed_of_light = cc.c.cgs.value
@@ -8,3 +9,6 @@ solar_mass = cc.M_sun.cgs.value
 sigma_sb = cc.sigma_sb.cgs.value
 radiation_constant = sigma_sb*4 / speed_of_light
 boltzmann_constant = cc.k_B.cgs.value
+km_cgs = uu.km.cgs.scale
+day_to_s = 86400
+au_cgs = uu.au.cgs.scale
