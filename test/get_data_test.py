d77107d64249ef5ad1eaecce7520dab0f7f80ab5
Moritz Huebner <email@moritz-huebner.de>
2022-02-27 22:31:15 +1100

    Fixed testing issues

--- test/get_data_test.py
+++ test/get_data_test.py
@@ -361,109 +361,99 @@ class TestSwiftDataGetter(unittest.TestCase):
 
 
 def _delete_downloaded_files():
-    shutil.rmtree('GRBData', ignore_errors=True)
-    shutil.rmtree('kilonova', ignore_errors=True)
-    shutil.rmtree('supernova', ignore_errors=True)
-    shutil.rmtree('tidal_disruption_event', ignore_errors=True)
-
-
-class DeleteDownloadedFiles(object):
-
-    def __enter__(self):
-        pass
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        _delete_downloaded_files()
-
-
-def delete_downloaded_files(func):
-    def wrapper(*args, **kwargs):
-        with DeleteDownloadedFiles():
-            func(*args, **kwargs)
-    return wrapper
+    for folder in ["GRBData", "kilonova", "supernova", "tidal_disruption_event"]:
+        shutil.rmtree(folder, ignore_errors=True)
 
 
 class TestReferenceFiles(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        _delete_downloaded_files()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        _delete_downloaded_files()
 
     def setUp(self) -> None:
         self.downloaded_file = ""
 
     def tearDown(self) -> None:
+        _delete_downloaded_files()
         del self.downloaded_file
 
     @property
     def reference_file(self):
         return f"reference_data/{self.downloaded_file}"
 
-    @delete_downloaded_files
+    def _compare_files_line_by_line(self):
+        with open(self.reference_file, 'r') as rf:
+            with open(self.downloaded_file, 'r') as df:
+                for l1, l2 in zip(rf.readlines(), df.readlines()):
+                    self.assertEqual(l1, l2)
+
     def test_swift_afterglow_flux_data(self):
         redback.get_data.get_afterglow_data_from_swift(grb='GRB070809', data_mode='flux')
         self.downloaded_file = "GRBData/afterglow/flux/GRB070809_rawSwiftData.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
         self.downloaded_file = "GRBData/afterglow/flux/GRB070809.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
-    @delete_downloaded_files
     def test_swift_xrt_flux_data(self):
         redback.get_data.get_xrt_data_from_swift(grb='GRB070809', data_mode='flux')
         self.downloaded_file = "GRBData/afterglow/flux/GRB070809_xrt_rawSwiftData.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
         self.downloaded_file = "GRBData/afterglow/flux/GRB070809_xrt.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
-    @delete_downloaded_files
     def test_swift_prompt_data(self):
         bin_size = "1s"
         redback.get_data.get_prompt_data_from_swift('GRB070809', bin_size=bin_size)
         self.downloaded_file = f"GRBData/prompt/flux/GRB070809_{bin_size}_lc.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
-    @delete_downloaded_files
     def test_open_catalog_kilonova_data(self):
         redback.get_data.get_open_transient_catalog_data(transient="at2017gfo", transient_type="kilonova")
 
         self.downloaded_file = f"kilonova/flux_density/at2017gfo.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
         self.downloaded_file = f"kilonova/flux_density/at2017gfo_metadata.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
         self.downloaded_file = f"kilonova/flux_density/at2017gfo_rawdata.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
-    @delete_downloaded_files
     def test_open_catalog_supernova_data(self):
         redback.get_data.get_open_transient_catalog_data(transient="SN2011kl", transient_type="supernova")
 
         self.downloaded_file = f"supernova/flux_density/SN2011kl.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
         self.downloaded_file = f"supernova/flux_density/SN2011kl_metadata.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
         self.downloaded_file = f"supernova/flux_density/SN2011kl_rawdata.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
-    @delete_downloaded_files
     def test_open_catalog_tde_data(self):
         redback.get_data.get_open_transient_catalog_data(transient="PS18kh", transient_type="tidal_disruption_event")
 
         self.downloaded_file = f"tidal_disruption_event/flux_density/PS18kh.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
         self.downloaded_file = f"tidal_disruption_event/flux_density/PS18kh_metadata.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
         self.downloaded_file = f"tidal_disruption_event/flux_density/PS18kh_rawdata.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
-    @delete_downloaded_files
     def test_batse_prompt_data(self):
         redback.get_data.get_prompt_data_from_batse(grb="000526")
         self.downloaded_file = "GRBData/prompt/flux/GRB000526_BATSE_lc.csv"
-        self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
+        self._compare_files_line_by_line()
 
         self.downloaded_file = "GRBData/prompt/flux/tte_bfits_8121.fits.gz"
         self.assertTrue(filecmp.cmp(self.reference_file, self.downloaded_file))
