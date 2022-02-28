86ef81849ac54aa50c73326573e981eea0ee3adf
Nikhil Sarin <nikhil.sarin@monash.edu>
2022-02-28 22:08:27 +0100

    Add new files to init for SED/photosphere/interaction processes

--- redback/__init__.py
+++ redback/__init__.py
@@ -1,3 +1,3 @@
-from redback import constants, get_data, redback_errors, priors, result, sampler, transient, transient_models, utils
+from redback import constants, get_data, redback_errors, priors, result, sampler, transient, transient_models, utils, photosphere, sed, interaction_processes
 from redback.transient import afterglow, kilonova, prompt, supernova, tde
 from redback.sampler import fit_model
