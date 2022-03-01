55c3de87b82bfcd45baeaf5f0518ed49ad8ed783
Nikhil Sarin <nikhil.sarin@monash.edu>
2022-03-01 17:44:58 +0100

    magnetar + nickel model added

--- redback/transient_models/supernova_models.py
+++ redback/transient_models/supernova_models.py
@@ -44,6 +44,7 @@ def sn_exponential_powerlaw(time, redshift, lbol_0, alpha_1, alpha_2, tpeak_d,
                             sed=sed.Blackbody,**kwargs):
     """
     :param time: observer frame time in days
+    :param redshift: source redshift
     :param lbol_0: bolometric luminosity scale in cgs
     :param alpha_1: first exponent
     :param alpha_2: second exponent
@@ -118,6 +119,7 @@ def arnett(time, redshift, f_nickel, mej, interaction_process=ip.Diffusion,
            sed=sed.Blackbody, **kwargs):
     """
     :param time: time in days
+    :param redshift: source redshift
     :param f_nickel: fraction of nickel mass
     :param mej: total ejecta mass in solar masses
     :param interaction_process: Default is Diffusion.
@@ -186,6 +188,7 @@ def basic_magnetar_powered(time, redshift, p0, bp, mass_ns, theta_pb, interactio
                             photosphere=photosphere.TemperatureFloor, sed=sed.Blackbody,**kwargs):
     """
     :param time: time in days in observer frame
+    :param redshift: source redshift
     :param p0: initial spin period
     :param bp: polar magnetic field strength in Gauss
     :param mass_ns: mass of neutron star in solar masses
@@ -218,8 +221,7 @@ def basic_magnetar_powered(time, redshift, p0, bp, mass_ns, theta_pb, interactio
         return flux_density.to(uu.ABmag).value
 
 @citation_wrapper('redback')
-def slsn_bolometric(time, p0, bp, mass_ns, theta_pb,
-                                       interaction_process=ip.Diffusion, **kwargs):
+def slsn_bolometric(time, p0, bp, mass_ns, theta_pb,interaction_process=ip.Diffusion, **kwargs):
     """
     Same as basic magnetar_powered but with constraint on rotational_energy/kinetic_energy and nebula phase
     :param time: time in days in source frame
@@ -239,10 +241,49 @@ def slsn_bolometric(time, p0, bp, mass_ns, theta_pb,
 
 @citation_wrapper('https://ui.adsabs.harvard.edu/abs/2017ApJ...850...55N/abstract')
 def slsn(time, redshift, p0, bp, mass_ns, theta_pb, interaction_process=ip.Diffusion,
-                            photosphere=photosphere.TemperatureFloor, sed=sed.Blackbody,**kwargs):
+         photosphere=photosphere.TemperatureFloor, sed=sed.CutoffBlackbody,**kwargs):
     """
     Same as basic magnetar_powered but with constraint on rotational_energy/kinetic_energy and nebula phase
     :param time: time in days in observer frame
+    :param redshift: source redshift
+    :param p0: initial spin period
+    :param bp: polar magnetic field strength in Gauss
+    :param mass_ns: mass of neutron star in solar masses
+    :param theta_pb: angle between spin and magnetic field axes
+    :param interaction_process: Default is Diffusion.
+            Can also be None in which case the output is just the raw engine luminosity
+    :param photosphere: Default is TemperatureFloor.
+            kwargs must have vej or relevant parameters if using different photosphere model
+    :param sed: Default is CutoffBlackbody.
+    :param kwargs: Must be all the kwargs required by the specific interaction_process, photosphere, sed methods used
+             e.g., for Diffusion and TemperatureFloor: kappa, kappa_gamma, vej (km/s), temperature_floor
+    :return: flux_density or magnitude depending on output_format kwarg
+    """
+    frequency = kwargs['frequency']
+    cutoff_wavelength = kwargs.get('cutoff_wavelength', 3000)
+    frequency, time = calc_kcorrected_properties(frequencies=frequency, redshift=redshift, time=time)
+    dl = cosmo.luminosity_distance(redshift).cgs.value
+    lbol = slsn_bolometric(time=time, p0=p0, bp=bp, mass_ns=mass_ns, theta_pb=theta_pb,
+                           interaction_process=interaction_process)
+    photo = photosphere(time=time, luminosity=lbol, **kwargs)
+    sed_1 = sed(temperature=photo.photosphere_temperature, r_photosphere=photo.r_photosphere,
+                frequencies=frequency, luminosity_distance=dl, cutoff_wavelength=cutoff_wavelength)
+
+    flux_density = sed_1.flux_density
+
+    if kwargs['output_format'] == 'flux_density':
+        return flux_density.to(uu.mJy).value
+    elif kwargs['output_format'] == 'magnitude':
+        return flux_density.to(uu.ABmag).value
+
+@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2018ApJS..236....6G/abstract')
+def magnetar_nickel(time, redshift, f_nickel, mej, p0, bp, mass_ns, theta_pb, interaction_process=ip.Diffusion,
+                    photosphere=photosphere.TemperatureFloor, sed=sed.Blackbody, **kwargs):
+    """
+    :param time: time in days in observer frame
+    :param f_nickel: fraction of nickel mass
+    :param mej: total ejecta mass in solar masses
+    :param redshift: source redshift
     :param p0: initial spin period
     :param bp: polar magnetic field strength in Gauss
     :param mass_ns: mass of neutron star in solar masses
@@ -256,12 +297,29 @@ def slsn(time, redshift, p0, bp, mass_ns, theta_pb, interaction_process=ip.Diffu
              e.g., for Diffusion and TemperatureFloor: kappa, kappa_gamma, vej (km/s), temperature_floor
     :return: flux_density or magnitude depending on output_format kwarg
     """
-    return basic_magnetar_powered(time=time, redshift=redshift, p0=p0, bp=bp, mass_ns=mass_ns,theta_pb=theta_pb,
-                                  interaction_process=interaction_process, photosphere=photosphere, sed=sed, **kwargs)
+    frequency = kwargs['frequency']
+    frequency, time = calc_kcorrected_properties(frequencies=frequency, redshift=redshift, time=time)
+    dl = cosmo.luminosity_distance(redshift).cgs.value
 
-@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2018ApJS..236....6G/abstract')
-def magnetar_nickel():
-    pass
+    lbol_mag = _basic_magnetar(time=time*86400, p0=p0, bp=bp, mass_ns=mass_ns, theta_pb=theta_pb)
+    lbol_arnett = _nickelcobalt_engine(time=time, f_nickel=f_nickel, mej=mej)
+    lbol = lbol_mag + lbol_arnett
+
+    if interaction_process is not None:
+        interaction_class = interaction_process(time=time, luminosity=lbol, **kwargs)
+        lbol = interaction_class.new_luminosity
+
+    photo = photosphere(time=time, luminosity=lbol, **kwargs)
+
+    sed_1 = sed(temperature=photo.photosphere_temperature, r_photosphere=photo.r_photosphere,
+                frequencies=frequency, luminosity_distance=dl)
+
+    flux_density = sed_1.flux_density
+
+    if kwargs['output_format'] == 'flux_density':
+        return flux_density.to(uu.mJy).value
+    elif kwargs['output_format'] == 'magnitude':
+        return flux_density.to(uu.ABmag).value
 
 @citation_wrapper('https://ui.adsabs.harvard.edu/abs/2013ApJ...773...76C/abstract')
 def csm_interaction():
