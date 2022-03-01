c6be9ceae94f10ecae130ce2e5844ffd37cc076c
Nikhil Sarin <nikhil.sarin@monash.edu>
2022-03-01 17:26:44 +0100

    Added some constraint functions

--- redback/__init__.py
+++ redback/__init__.py
@@ -1,3 +1,4 @@
-from redback import constants, get_data, redback_errors, priors, result, sampler, transient, transient_models, utils, photosphere, sed, interaction_processes
+from redback import constants, get_data, redback_errors, priors, result, sampler, transient, transient_models, \
+    utils, photosphere, sed, interaction_processes, constraints
 from redback.transient import afterglow, kilonova, prompt, supernova, tde
 from redback.sampler import fit_model
---
+++ redback/constraints.py
@@ -0,0 +1,58 @@
+import numpy as np
+from redback.constants import *
+import redback.transient_models as tm
+
+def superluminous_supernova_constraint(parameters):
+    converted_parameters = parameters.copy()
+    mej = parameters['mej'] * solar_mass
+    vej = parameters['vej'] * km_cgs
+    kappa = parameters['kappa']
+    mass_ns = parameters['mass_ns']
+    p0 = parameters['p0']
+    kinetic_energy = 0.5 * mej * vej**2
+    rotational_energy =  2.6e52 * (mass_ns/1.4)**(3./2.) * p0**(-2)
+    tnebula =  np.sqrt(3 * kappa * mej / (4 * np.pi * vej ** 2)) / 86400
+    neutrino_energy = 1e51
+    total_energy = kinetic_energy + neutrino_energy
+    # ensure rotational energy is greater than total output energy
+    converted_parameters['erot_constraint'] = rotational_energy - total_energy
+    # ensure t_nebula is greater than 100 days
+    converted_parameters['t_nebula_min'] = tnebula - 100
+    return converted_parameters
+
+
+def csm_constraints():
+    pass
+
+@np.vectorize
+def basic_magnetar_powered_sn_constraints():
+    pass
+
+@np.vectorize
+def general_magnetar_powered_sn_constraints():
+    pass
+
+
+@np.vectorize
+def tde_constraints():
+    pass
+
+@np.vectorize
+def magnetar_boosted_kilonova_constraints():
+    pass
+
+@np.vectorize
+def nuclear_burning_constraints():
+    pass
+
+@np.vectorize
+def simple_fallback_constraints():
+    pass
+
+@np.vectorize
+def eos_constraints():
+    pass
+
+@np.vectorize
+def max_mass_constraints():
+    pass
