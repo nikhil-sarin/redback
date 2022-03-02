eac6b43e2e8c22371efff9a8d183f613380cbe83
Moritz Huebner <email@moritz-huebner.de>
2022-03-02 22:12:25 +1100

    return the data

--- redback/get_data/__init__.py
+++ redback/get_data/__init__.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from typing import Union
 import urllib
 
+import pandas as pd
+
 from redback.get_data import batse, directory, fermi, konus, open_data, swift, utils
 from redback.get_data.swift import SwiftDataGetter
 from redback.get_data.open_data import OpenDataGetter
@@ -17,7 +19,7 @@ DATA_SOURCES = ["swift", "swift_xrt", "fermi", "konus", "batse", "open_data"]
 TRANSIENT_TYPES = ["afterglow", "prompt", "kilonova", "supernova", "tidal_disruption_event"]
 
 
-def get_xrt_afterglow_data_from_swift(grb: str, data_mode: str = None, **kwargs: dict) -> SwiftDataGetter:
+def get_xrt_afterglow_data_from_swift(grb: str, data_mode: str = None, **kwargs: dict) -> pd.DataFrame:
     """
     Get XRT afterglow data from Swift. Creates a directory structure and saves the data.
     Returns the getter, though no further action needs to be taken by the user.
@@ -33,13 +35,13 @@ def get_xrt_afterglow_data_from_swift(grb: str, data_mode: str = None, **kwargs:
 
     Returns
     -------
-    SwiftDataGetter: The getter can help debug issues.
+    pandas.DataFrame: The processed data.
 
     """
     return get_swift_data(grb=grb, transient_type='afterglow', data_mode=data_mode, instrument="XRT")
 
 
-def get_bat_xrt_afterglow_data_from_swift(grb: str, data_mode: str, **kwargs: dict) -> SwiftDataGetter:
+def get_bat_xrt_afterglow_data_from_swift(grb: str, data_mode: str, **kwargs: dict) -> pd.DataFrame:
     """
     Get BAT+XRT afterglow data from Swift. Creates a directory structure and saves the data.
     Returns the getter, though no further action needs to be taken by the user.
@@ -55,13 +57,13 @@ def get_bat_xrt_afterglow_data_from_swift(grb: str, data_mode: str, **kwargs: di
 
     Returns
     -------
-    SwiftDataGetter: The getter can help debug issues.
+    pandas.DataFrame: The processed data.
 
     """
     return get_swift_data(grb=grb, transient_type='afterglow', data_mode=data_mode, instrument="BAT+XRT")
 
 
-def get_prompt_data_from_swift(grb: str, bin_size: str = "1s", **kwargs: dict) -> SwiftDataGetter:
+def get_prompt_data_from_swift(grb: str, bin_size: str = "1s", **kwargs: dict) -> pd.DataFrame:
     """
     Get prompt emission data from Swift. Creates a directory structure and saves the data.
     Returns the getter, though no further action needs to be taken by the user.
@@ -77,7 +79,7 @@ def get_prompt_data_from_swift(grb: str, bin_size: str = "1s", **kwargs: dict) -
 
     Returns
     -------
-    SwiftDataGetter: The getter can help debug issues.
+    pandas.DataFrame: The processed data.
 
     """
     return get_swift_data(grb=grb, transient_type='prompt', data_mode='prompt', instrument="BAT+XRT", bin_size=bin_size)
@@ -85,7 +87,7 @@ def get_prompt_data_from_swift(grb: str, bin_size: str = "1s", **kwargs: dict) -
 
 def get_swift_data(
         grb: str, transient_type: str, data_mode: str = 'flux', instrument: str = 'BAT+XRT',
-        bin_size: str = None, **kwargs: dict) -> SwiftDataGetter:
+        bin_size: str = None, **kwargs: dict) -> pd.DataFrame:
     """
     Catch all data getting function for Swift.  Creates a directory structure and saves the data.
     Returns the getter, though no further action needs to be taken by the user.
@@ -107,17 +109,16 @@ def get_swift_data(
 
     Returns
     -------
-    SwiftDataGetter: The getter can help debug issues.
+    pandas.DataFrame: The processed data.
 
     """
     getter = SwiftDataGetter(
         grb=grb, transient_type=transient_type, data_mode=data_mode,
         bin_size=bin_size, instrument=instrument)
-    getter.get_data()
-    return getter.data
+    return getter.get_data()
 
 
-def get_prompt_data_from_batse(grb: str, **kwargs) -> BATSEDataGetter:
+def get_prompt_data_from_batse(grb: str, **kwargs) -> pd.DataFrame:
     """
     Get prompt emission data from BATSE. Creates a directory structure and saves the data.
     Returns the getter, though no further action needs to be taken by the user.
@@ -131,15 +132,14 @@ def get_prompt_data_from_batse(grb: str, **kwargs) -> BATSEDataGetter:
 
     Returns
     -------
-    BATSEDataGetter: The getter can help debug issues.
+    pandas.DataFrame: The processed data.
 
     """
     getter = BATSEDataGetter(grb=grb)
-    getter.get_data()
-    return getter.data
+    return getter.get_data()
 
 
-def get_kilonova_data_from_open_transient_catalog_data(transient: str, **kwargs: dict) -> OpenDataGetter:
+def get_kilonova_data_from_open_transient_catalog_data(transient: str, **kwargs: dict) -> pd.DataFrame:
     """
     Get kilonova data from the Open Access Catalog. Creates a directory structure and saves the data.
     Returns the getter, though no further action needs to be taken by the user.
@@ -153,13 +153,13 @@ def get_kilonova_data_from_open_transient_catalog_data(transient: str, **kwargs:
 
     Returns
     -------
-    OpenDataGetter: The getter can help debug issues.
+    pandas.DataFrame: The processed data.
 
     """
     return get_open_transient_catalog_data(transient, transient_type="kilonova")
 
 
-def get_supernova_data_from_open_transient_catalog_data(transient: str, **kwargs: dict) -> OpenDataGetter:
+def get_supernova_data_from_open_transient_catalog_data(transient: str, **kwargs: dict) -> pd.DataFrame:
     """
     Get supernova data from the Open Access Catalog. Creates a directory structure and saves the data.
     Returns the getter, though no further action needs to be taken by the user.
@@ -173,14 +173,14 @@ def get_supernova_data_from_open_transient_catalog_data(transient: str, **kwargs
 
     Returns
     -------
-    OpenDataGetter: The getter can help debug issues.
+    pandas.DataFrame: The processed data.
 
     """
     return get_open_transient_catalog_data(transient, transient_type="supernova")
 
 
 def get_tidal_disruption_event_data_from_open_transient_catalog_data(
-        transient: str, **kwargs: dict) -> OpenDataGetter:
+        transient: str, **kwargs: dict) -> pd.DataFrame:
     """
     Get TDE data from the Open Access Catalog. Creates a directory structure and saves the data.
     Returns the getter, though no further action needs to be taken by the user.
@@ -194,13 +194,13 @@ def get_tidal_disruption_event_data_from_open_transient_catalog_data(
 
     Returns
     -------
-    OpenDataGetter: The getter can help debug issues.
+    pandas.DataFrame: The processed data.
 
     """
     return get_open_transient_catalog_data(transient, transient_type="tidal_disruption_event")
 
 
-def get_prompt_data_from_fermi(*args: list, **kwargs: dict) -> FermiDataGetter:
+def get_prompt_data_from_fermi(*args: list, **kwargs: dict) -> pd.DataFrame:
     """
     Get prompt emission data from Fermi. Creates a directory structure and saves the data.
     Returns the getter, though no further action needs to be taken by the user.
@@ -219,7 +219,7 @@ def get_prompt_data_from_fermi(*args: list, **kwargs: dict) -> FermiDataGetter:
     raise NotImplementedError("This function is not yet implemented.")
 
 
-def get_prompt_data_from_konus(*args: list, **kwargs: dict) -> KonusDataGetter:
+def get_prompt_data_from_konus(*args: list, **kwargs: dict) -> pd.DataFrame:
     """
     Get prompt emission data from Konus. Creates a directory structure and saves the data.
     Returns the getter, though no further action needs to be taken by the user.
@@ -239,7 +239,7 @@ def get_prompt_data_from_konus(*args: list, **kwargs: dict) -> KonusDataGetter:
 
 
 def get_open_transient_catalog_data(
-        transient: str, transient_type: str, **kwargs) -> OpenDataGetter:
+        transient: str, transient_type: str, **kwargs) -> pd.DataFrame:
     """
     Catch all data getting function for the Open Access Catalog. Creates a directory structure and saves the data.
     Returns the getter, though no further action needs to be taken by the user.
@@ -286,7 +286,7 @@ _functions_dict = {
 
 def get_data(
         transient: str, instrument: str, **kwargs: dict)\
-        -> Union[SwiftDataGetter, OpenDataGetter, BATSEDataGetter, FermiDataGetter, KonusDataGetter]:
+        -> pd.DataFrame:
     """
     Catch all data getter.
 
@@ -301,8 +301,7 @@ def get_data(
 
     Returns
     -------
-    Union[SwiftDataGetter, OpenDataGetter, BATSEDataGetter, FermiDataGetter, KonusDataGetter]:
-        The data getter can be helpful when debugging
+    pandas.DataFrame: The processed data.
 
     """
     return _functions_dict[(transient, instrument)](transient, **kwargs)
