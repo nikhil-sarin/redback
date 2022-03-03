9863e1b859a9494e1841a1060d382c728be6d80a
Moritz Huebner <email@moritz-huebner.de>
2022-03-03 19:47:11 +1100

    Added direct access to transient classes

--- redback/transient/__init__.py
+++ redback/transient/__init__.py
@@ -1,5 +1,12 @@
 from redback.transient import afterglow, kilonova, prompt, supernova, tde, transient
+from afterglow import Afterglow, LGRB, SGRB
+from kilonova import Kilonova
+from prompt import PromptTimeSeries
+from supernova import Supernova
+from tde import TDE
+from transient import Transient
 
-TRANSIENT_DICT = dict(afterglow=afterglow.Afterglow, lgrb=afterglow.LGRB, sgrb=afterglow.SGRB,
-                      kilonova=kilonova.Kilonova, prompt=prompt.PromptTimeSeries, supernova=supernova.Supernova,
-                      tde=tde.TDE, transient=transient.Transient)
+
+TRANSIENT_DICT = dict(
+    afterglow=Afterglow, lgrb=LGRB, sgrb=SGRB, kilonova=Kilonova, prompt=PromptTimeSeries, supernova=Supernova,
+    tde=TDE, transient=Transient)
