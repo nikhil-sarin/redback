e1b51ff893535149c6785ec3d5e60aff72179a16
Moritz Huebner <email@moritz-huebner.de>
2022-03-10 23:31:53 +1100

    Fixed some priors

--- test/prior_test.py
+++ test/prior_test.py
@@ -6,16 +6,25 @@ from os import listdir
 class TestLoadPriors(unittest.TestCase):
 
     def setUp(self) -> None:
-        pass
+        self.path_to_files = "../redback/priors/"
+        self.prior_files = listdir(self.path_to_files)
 
     def tearDown(self) -> None:
         pass
 
     def test_load_priors(self):
-        path_to_files = "../redback/priors/"
-        prior_files = listdir(path_to_files)
+        for f in self.prior_files:
+            prior_dict = bilby.prior.PriorDict()
+            prior_dict.from_file(f"{self.path_to_files}{f}")
 
-        for f in prior_files:
+    def test_dollar_signs(self):
+        for f in self.prior_files:
+            print()
             print(f)
             prior_dict = bilby.prior.PriorDict()
-            prior_dict.from_file(f"{path_to_files}{f}")
+            prior_dict.from_file(f"{self.path_to_files}{f}")
+            for k, p in prior_dict.items():
+                print(k)
+                occurences = p.latex_label.count("$")
+                assert occurences % 2 == 0
+
