1349a44c9b4ebda3009753f517567b2d5974029b
Moritz Huebner <email@moritz-huebner.de>
2022-04-04 22:39:33 +1000

    Refactored line and moved `flux_density` up in a class hierarchy

--- redback/sed.py
+++ redback/sed.py
@@ -1,6 +1,9 @@
+from typing import Union
+
 import numpy as np
 from redback.constants import *
 from redback.utils import nu_to_lambda, lambda_to_nu
+
 
 def blackbody_to_flux_density(temperature, r_photosphere, dl, frequency):
     """
@@ -26,93 +29,126 @@ def blackbody_to_flux_density(temperature, r_photosphere, dl, frequency):
     flux_density = num / denom * frac
     return flux_density
 
-class CutoffBlackbody(object):
-    def __init__(self, time, temperature, luminosity, r_photosphere,
-                 frequency, luminosity_distance, cutoff_wavelength, **kwargs):
+
+class _SED(object):
+
+    @property
+    def flux_density(self):
+        flux_density = self.sed.copy()
+        # add distance units
+        flux_density /= (4 * np.pi * self.luminosity_distance ** 2)
+        # get rid of Angstrom and normalise to frequency
+        flux_density *= nu_to_lambda(self.frequency)
+        flux_density /= self.frequency
+
+        # add units
+        flux_density = flux_density << self.UNITS
+
+        # convert to mJy
+        return flux_density.to(uu.mJy)
+
+class CutoffBlackbody(_SED):
+
+    X_CONST = planck * speed_of_light / boltzmann_constant
+    FLUX_CONST = 4 * np.pi * 2 * np.pi * planck * speed_of_light ** 2 * angstrom_cgs
+    # sed units are erg/s/Angstrom - need to turn them into flux density compatible units
+    UNITS = uu.erg / uu.s / uu.Hz / uu.cm**2.
+
+    reference = "https://ui.adsabs.harvard.edu/abs/2017ApJ...850...55N/abstract"
+
+    def __init__(self, time: np.ndarray, temperature: np.ndarray, luminosity: np.ndarray, r_photosphere: np.ndarray,
+                 frequency: Union[float, np.ndarray], luminosity_distance: float, cutoff_wavelength: float,
+                 **kwargs: None) -> None:
         """
         Blackbody SED with a cutoff
 
         :param time: time in source frame in seconds
-        :param luminosity: luminosity in cgs
         :param temperature: temperature in kelvin
+        :param luminosity: luminosity in cgs
         :param r_photosphere: photosphere radius in cm
         :param frequency: frequency in Hz - must be a single number or same length as time array
         :param luminosity_distance: dl in cm
+        :param cutoff_wavelength: cutoff wavelength in Angstrom
         :param kwargs: None
         """
         self.time = time
+        self.unique_times = np.unique(self.time)
+        tsort = np.argsort(self.time)
+        self.uniq_is = np.searchsorted(self.time, self.unique_times, sorter=tsort)
+
         self.luminosity = luminosity
         self.temperature = temperature
         self.r_photosphere = r_photosphere
-        self.frequency = frequency
+        if isinstance(frequency, (float, int)):
+            self.frequency = np.array([frequency])
+        else:
+            self.frequency = frequency
         self.luminosity_distance = luminosity_distance
-        self.cutoff_wavelength = cutoff_wavelength
-        self.reference = 'https://ui.adsabs.harvard.edu/abs/2017ApJ...850...55N/abstract'
+        self.cutoff_wavelength = cutoff_wavelength * angstrom_cgs
 
-        self.sed = []
-        self.flux_density = self.calculate_flux_density()
+        self.norms = None
+
+        self.sed = np.zeros(len(self.time))
+        self.calculate_flux_density()
+
+    @property
+    def wavelength(self):
+        return nu_to_lambda(self.frequency)
+
+    @property
+    def mask(self):
+        return self.wavelength < self.cutoff_wavelength
+
+    @property
+    def nxcs(self):
+        return self.X_CONST * np.array(range(1, 11))
+
+    def _set_sed(self):
+        self.sed[self.mask] = \
+            self.FLUX_CONST * (self.r_photosphere[self.mask]**2 / self.cutoff_wavelength /
+                               self.wavelength[self.mask] ** 4) \
+            / np.expm1(self.X_CONST / self.wavelength[self.mask] / self.temperature[self.mask])
+        self.sed[~self.mask] = \
+            self.FLUX_CONST * (self.r_photosphere[~self.mask]**2 / self.wavelength[~self.mask]**5) \
+            / np.expm1(self.X_CONST / self.wavelength[~self.mask] / self.temperature[~self.mask])
+        # Apply renormalisation
+        self.sed *= self.norms[np.searchsorted(self.unique_times, self.time)]
+
+    def _set_norm(self):
+        self.norms = self.luminosity[self.uniq_is] / \
+                     (self.FLUX_CONST / angstrom_cgs * self.r_photosphere[self.uniq_is] ** 2 * self.temperature[
+                         self.uniq_is])
+
+        tp = self.temperature[self.uniq_is].reshape(len(self.unique_times), 1)
+        tp2 = tp ** 2
+        tp3 = tp ** 3
+
+        c1 = np.exp(-self.nxcs / (self.cutoff_wavelength * tp))
+
+        term_1 = \
+            c1 * (self.nxcs ** 2 + 2 * (self.nxcs * self.cutoff_wavelength * tp + self.cutoff_wavelength ** 2 * tp2)) \
+            / (self.nxcs ** 3 * self.cutoff_wavelength ** 3)
+        term_2 = \
+            (6 * tp3 - c1 *
+             (self.nxcs ** 3 + 3 * self.nxcs ** 2 * self.cutoff_wavelength * tp + 6 *
+              (self.nxcs * self.cutoff_wavelength ** 2 * tp2 + self.cutoff_wavelength ** 3 * tp3))
+             / self.cutoff_wavelength ** 3) / self.nxcs ** 4
+        f_blue_reds = np.sum(term_1 + term_2, 1)
+        self.norms /= f_blue_reds
 
     def calculate_flux_density(self):
         # Mostly from Mosfit/SEDs
-        cutoff_wavelength = self.cutoff_wavelength * angstrom_cgs
-        wavelength = nu_to_lambda(self.frequency)
-        x_const = planck * speed_of_light / boltzmann_constant
-        flux_const = 4 * np.pi * 2*np.pi * planck * speed_of_light**2 * angstrom_cgs
-
-        mask = wavelength < cutoff_wavelength
-        sed = np.zeros(len(self.time))
-
-        sed[mask] = flux_const * (self.r_photosphere[mask]**2 / cutoff_wavelength / wavelength[mask] ** 4) \
-                    / np.expm1(x_const / wavelength[mask] / self.temperature[mask])
-        sed[~mask] = flux_const * (self.r_photosphere[~mask]**2 / wavelength[~mask]**5) \
-                     / np.expm1(x_const / wavelength[~mask] / self.temperature[~mask])
-
-        uniq_times = np.unique(self.time)
-        tsort = np.argsort(self.time)
-        uniq_is = np.searchsorted(self.time, uniq_times, sorter=tsort)
-        lu = len(uniq_times)
-
-        norms = self.luminosity[uniq_is] / \
-                (flux_const / angstrom_cgs * self.r_photosphere[uniq_is]**2 * self.temperature[uniq_is])
-
-        tp = self.temperature[uniq_is].reshape(lu, 1)
-        tp2 = tp**2
-        tp3 = tp**3
-        nxcs = x_const * np.array(range(1, 11))
-                
-        f_blue_reds = \
-            np.sum(
-                np.exp(-nxcs / (cutoff_wavelength * tp)) * (nxcs ** 2 + 2 * (nxcs * cutoff_wavelength * tp + cutoff_wavelength**2 * tp2)) / (nxcs ** 3 * cutoff_wavelength**3) +
-                (
-                    (6 * tp3 - np.exp(-nxcs / (cutoff_wavelength * tp)) * (nxcs ** 3 + 3 * nxcs ** 2 * cutoff_wavelength * tp + 6 * (nxcs * cutoff_wavelength**2 * tp2 + cutoff_wavelength**3 *tp3)) / cutoff_wavelength**3) / (nxcs ** 4)
-                ), 1
-            )
-        norms /= f_blue_reds
-
-        # Apply renormalisation
-        sed *= norms[np.searchsorted(uniq_times, self.time)]
-
-        self.sed = sed
-
-        # sed units are erg/s/Angstrom - need to turn them into flux density compatible units
-        units = uu.erg / uu.s / uu.Hz / uu.cm**2.
-
-        # add distance units
-        sed = sed / (4*np.pi * self.luminosity_distance**2)
-        # get rid of Angstrom and normalise to frequency
-        sed = sed * nu_to_lambda(self.frequency)
-        sed = sed / self.frequency
-
-        # add units
-        flux_density = sed << units
-
-        # convert to mJy
-        flux_density = flux_density.to(uu.mJy)
-        return flux_density
+        self._set_norm()
+        self._set_sed()
+        return self.flux_density
 
 
 class Blackbody(object):
-    def __init__(self, temperature, r_photosphere, frequency, luminosity_distance, **kwargs):
+
+    reference = "It is a blackbody - Do you really need a reference for this?"
+
+    def __init__(self, temperature: np.ndarray, r_photosphere: np.ndarray, frequency: np.ndarray,
+                 luminosity_distance: float, **kwargs: None) -> None:
         """
         Simple Blackbody SED
 
@@ -126,23 +162,29 @@ class Blackbody(object):
         self.r_photosphere = r_photosphere
         self.frequency = frequency
         self.luminosity_distance = luminosity_distance
-        self.reference = 'It is a blackbody - Do you really need a reference for this?'
 
         self.flux_density = self.calculate_flux_density()
 
     def calculate_flux_density(self):
-        flux_density = blackbody_to_flux_density(temperature=self.temperature, r_photosphere=self.r_photosphere,
-                                                 frequency=self.frequency, dl=self.luminosity_distance)
-        return flux_density
+        self.flux_density = blackbody_to_flux_density(
+            temperature=self.temperature, r_photosphere=self.r_photosphere,
+            frequency=self.frequency, dl=self.luminosity_distance)
+        return self.flux_density
 
 
-class Synchrotron(object):
-    def __init__(self, frequency, luminosity_distance,
-                 pp,nu_max, source_radius=1e13, f0=1e-26, **kwargs):
+class Synchrotron(_SED):
+
+    reference = "https://ui.adsabs.harvard.edu/abs/2004rvaa.conf...13H/abstract"
+    # sed units are erg/s/Angstrom - need to turn them into flux density compatible units
+    UNITS = uu.erg / uu.s / uu.Hz / uu.cm**2
+
+    def __init__(self, frequency: Union[np.ndarray, float, int], luminosity_distance: float, pp: float, nu_max: float,
+                 source_radius: float = 1e13, f0: float = 1e-26, **kwargs: None) -> None:
         """
         Synchrotron SED
 
-        :param frequency: frequency to calculate in Hz - Must be same length as time array or a single number. In source frame
+        :param frequency: frequency to calculate in Hz - Must be same length as time array or a single number.
+                          In source frame.
         :param luminosity_distance: luminosity_distance in cm
         :param pp: synchrotron power law slope
         :param nu_max: max frequency
@@ -150,44 +192,47 @@ class Synchrotron(object):
         :param f0: frequency normalization
         :param kwargs: None
         """
-        self.frequency = frequency
+        if isinstance(frequency, (float, int)):
+            self.frequency = np.array([frequency])
+        else:
+            self.frequency = frequency
         self.luminosity_distance = luminosity_distance
         self.pp = pp
         self.nu_max = nu_max
         self.source_radius = source_radius
         self.f0 = f0
-        self.reference = 'https://ui.adsabs.harvard.edu/abs/2004rvaa.conf...13H/abstract'
-        self.sed = []
+        self.sed = None
 
-        self.flux_density = self.calculate_flux_density()
+        self.calculate_flux_density()
+
+    @property
+    def f_max(self):
+        return self.f0 * self.source_radius**2 * self.nu_max ** 2.5  # for SSA
+
+    @property
+    def mask(self):
+        return self.frequency < self.nu_max
+
+    def _set_sed(self):
+        self.sed = np.zeros(len(self.frequency))
+        self.sed[self.mask] = \
+            self.f0 * self.source_radius**2 * (self.frequency[self.mask]/self.nu_max) ** 2.5 \
+            * angstrom_cgs / speed_of_light * self.frequency[self.mask] ** 2
+        self.sed[~self.mask] = \
+            self.f_max * (self.frequency[~self.mask]/self.nu_max)**(-(self.pp - 1.)/2.) \
+            * angstrom_cgs / speed_of_light * self.frequency[~self.mask] **2
 
     def calculate_flux_density(self):
-        fmax = self.f0 * self.source_radius**2 * self.nu_max ** 2.5 # for SSA
-        mask = self.frequency < self.nu_max
-        sed = np.zeros(len(self.frequency))
+        self._set_sed()
+        return self.flux_density
 
-        # sed units are erg/s/Angstrom - need to turn them into flux density compatible units
-        units = uu.erg / uu.s / uu.hz / uu.cm**2
 
-        sed[mask] = self.f0 * self.source_radius**2 * \
-                    (self.frequency/self.nu_max)**2.5 * angstrom_cgs / speed_of_light * self.frequency **2
-        sed[~mask] = fmax * (self.frequency/self.nu_max)**(-(self.pp - 1.)/2.) \
-                     * angstrom_cgs / speed_of_light * self.frequency **2
-        self.sed = sed
-        # add distance units
-        sed = sed / (4*np.pi * self.luminosity_distance**2)
-        # get rid of Angstrom and normalise to frequency
-        sed = sed * nu_to_lambda(self.frequency)
-        sed = sed / self.frequency
+class Line(_SED):
 
-        # add units
-        flux_density = sed << units
+    reference = "https://ui.adsabs.harvard.edu/abs/2018ApJS..236....6G/abstract"
+    # sed units are erg/s/Angstrom - need to turn them into flux density compatible units
+    UNITS = uu.erg / uu.s / uu.Hz / uu.cm**2
 
-        # convert to mJy
-        flux_density = flux_density.to(uu.mJy)
-        return flux_density
-
-class Line(object):
     def __init__(self, time, luminosity, frequency, sed, luminosity_distance, line_wavelength=7.5e3, line_width=500,
                  line_time=50, line_duration=25, line_amplitude=0.3, **kwargs):
         """
@@ -195,7 +240,8 @@ class Line(object):
 
         :param time: time in source frame
         :param luminosity: luminosity in cgs
-        :param frequency: frequency to calculate in Hz - Must be same length as time array or a single number. In source frame
+        :param frequency: frequency to calculate in Hz - Must be same length as time array or a single number.
+                          In source frame.
         :param sed: instantiated SED class object.
         :param luminosity_distance: luminosity_distance in cm
         :param line_wavelength: line wavelength in angstrom
@@ -209,6 +255,7 @@ class Line(object):
         self.luminosity = luminosity
         self.frequency = frequency
         self.SED = sed
+        self.sed = None
         self.luminosity_distance = luminosity_distance
         self.line_wavelength = line_wavelength
         self.line_width = line_width
@@ -216,35 +263,22 @@ class Line(object):
         self.line_duration = line_duration
         self.line_amplitude = line_amplitude
 
+        self.calculate_flux_density()
 
-        self.reference = 'https://ui.adsabs.harvard.edu/abs/2018ApJS..236....6G/abstract'
-
-        self.flux_density = self.calculate_flux_density()
+    @property
+    def wavelength(self):
+        return nu_to_lambda(self.frequency)
 
     def calculate_flux_density(self):
         # Mostly from Mosfit/SEDs
-        wavelength = nu_to_lambda(self.frequency)
-        amplitude = self.line_amplitude * np.exp(-0.5*((self.time - self.line_time)/self.line_duration)**2)
+        self._set_sed()
+        return self.flux_density
 
-        seds = self.SED.sed * (1 - amplitude)
-        amplitude *= self.luminosity / (self.line_width * (2*np.pi)**0.5)
+    def _set_sed(self):
+        amplitude = self.line_amplitude * np.exp(-0.5 * ((self.time - self.line_time) / self.line_duration) ** 2)
+        self.sed = self.SED.sed * (1 - amplitude)
 
-        amp_new = np.exp(-0.5 * ((wavelength - self.line_wavelength) / self.line_width)**2)
+        amplitude *= self.luminosity / (self.line_width * (2 * np.pi) ** 0.5)
 
-        seds += amplitude * amp_new
-
-        # sed units are erg/s/Angstrom - need to turn them into flux density compatible units
-        units = uu.erg / uu.s / uu.hz / uu.cm ** 2
-
-        # add distance units
-        seds = seds / (4*np.pi * self.luminosity_distance**2)
-        # get rid of Angstrom and normalise to frequency
-        seds = seds * nu_to_lambda(self.frequency)
-        seds = seds / self.frequency
-
-        # add units
-        flux_density = seds << units
-
-        # convert to mJy
-        flux_density = flux_density.to(uu.mJy)
-        return flux_density
+        amp_new = np.exp(-0.5 * ((self.wavelength - self.line_wavelength) / self.line_width) ** 2)
+        self.sed += amplitude * amp_new
