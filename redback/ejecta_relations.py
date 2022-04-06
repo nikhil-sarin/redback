c1ca88ef7b3de40dca87dc7adb66fdbd4f702d97
Nikhil Sarin <nikhil.sarin@monash.edu>
2022-04-06 15:13:24 +0200

    NSBH ejecta relation from Kawaguchi+2016

--- redback/ejecta_relations.py
+++ redback/ejecta_relations.py
@@ -2,23 +2,26 @@ import numpy as np
 from astropy import constants as cc
 
 
-class Dietrich_ujevic_17(object):
+class Dietrich_Ujevic_2017(object):
     """
-    Relations to connect intrinsic GW parameters to extrinsic kilonova parameters
+    Relations to connect intrinsic GW parameters to extrinsic kilonova parameters from Dietrich and Ujevic 2017
+    for a one component kilonova model
     """
     def __init__(self, mass_1, mass_2, lambda_1, lambda_2):
         self.mass_1 = mass_1
         self.mass_2 = mass_2
         self.lambda_1 = lambda_1
         self.lambda_2 = lambda_2
+        self.c1 = calc_compactness_from_lambda(self.lambda_1)
+        self.c2 = calc_compactness_from_lambda(self.lambda_2)
         self.reference = 'https://ui.adsabs.harvard.edu/abs/2017CQGra..34j5014D/abstract'
         self.ejecta_mass = self.calculate_ejecta_mass()
         self.ejecta_velocity = self.calculate_ejecta_velocity()
 
     @property
     def calculate_ejecta_velocity(self):
-        c1 = calc_compactness_from_lambda(self.lambda_1)
-        c2 = calc_compactness_from_lambda(self.lambda_2)
+        c1 = self.c1
+        c2 = self.c2
 
         a = -0.3090
         b = 0.657
@@ -28,8 +31,8 @@ class Dietrich_ujevic_17(object):
 
     @property
     def calculate_ejecta_mass(self):
-        c1 = calc_compactness_from_lambda(self.lambda_1)
-        c2 = calc_compactness_from_lambda(self.lambda_2)
+        c1 = self.c1
+        c2 = self.c2
 
         a = -0.0719
         b = 0.2116
@@ -40,6 +43,53 @@ class Dietrich_ujevic_17(object):
                     (self.mass_1 * (self.mass_2 / self.mass_1) ** n + self.mass_2 * (self.mass_1 / self.mass_2) ** n) + d
 
         mej = 10 ** log10_mej
+        return mej
+
+class Kawaguchi_nsbh_2016(object):
+    """
+    Relations to connect intrinsic GW parameters to extrinsic kilonova parameters
+    for a neutron star black hole merger with one component
+    """
+    def __init__(self, mass_bh, mass_ns, chi_eff, lambda_ns):
+        self.mass_bh = mass_bh
+        self.mass_ns = mass_ns
+        self.mass_ratio = mass_bh/mass_ns
+        self.chi_eff = chi_eff
+        self.lambda_ns = lambda_ns
+        self.reference = 'https://ui.adsabs.harvard.edu/abs/2016ApJ...825...52K/abstract'
+        self.risco = self.isco_radius()
+        self.ejecta_velocity = self.calculate_ejecta_velocity()
+        self.ejecta_mass = self.calculate_ejecta_mass()
+
+    @property
+    def isco_radius(self):
+        chi = self.chi_eff
+        z1 = 1 + ((1 - chi * chi) ** (1 / 3.0)) * (((1 + chi) ** (1 / 3.0)) + (1 - chi) ** (1 / 3.0))
+        z2 = (3 * chi * chi + z1 * z1) ** (1 / 2.0)
+        risco = 3 + z2 - np.sign(chi) * ((3 - z1) * (3 + z1 + 2 * z2)) ** (1 / 2.0)
+        return risco
+
+    @property
+    def calculate_ejecta_velocity(self):
+        vej = 1.5333330951369120e-2*self.mass_ratio+0.19066667068621043
+        return vej
+
+    @property
+    def calculate_ejecta_mass(self):
+        a1 = -2.269e-3
+        a2 = 4.464e-2
+        a3 = 2.431
+        a4 = -0.4159
+        n1 = 1.352
+        n2 = 0.2497
+
+        compactness = calc_compactness_from_lambda(self.lambda_ns)
+        baryonic_mass = calc_baryonic_mass(mass=self.mass_ns, compactness=compactness)
+
+        tmp1 = self.risco * (self.mass_ratio ** n1) * a1
+        tmp2 = (self.mass_ratio ** n2) * (1 - 2 * compactness) * a2 / compactness
+        tmp3 = (1 - self.mass_ns / baryonic_mass) * a3 + a4
+        mej = baryonic_mass * np.maximum(tmp1 + tmp2 + tmp3, 0)
         return mej
 
 
