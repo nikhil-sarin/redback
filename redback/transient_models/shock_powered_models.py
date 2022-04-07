704ee7121b10239a5579cbc242ea0286e858714e
Nikhil Sarin <nikhil.sarin@monash.edu>
2022-04-07 19:06:22 +0200

    stubs for functions to be added soon and their references

--- redback/transient_models/shock_powered_models.py
+++ redback/transient_models/shock_powered_models.py
@@ -1,11 +1,41 @@
-from redback.utils import calc_kcorrected_properties, citation_wrapper, logger, get_csm_properties, nu_to_lambda
+import numpy as np
+from redback.constants import *
+from redback.utils import calc_kcorrected_properties, citation_wrapper, logger
+
+@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2021ApJ...909..209P/abstract')
+def _shock_cooling():
+    pass
+
+@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2021ApJ...909..209P/abstract')
+def shock_cooling_bolometric():
+    pass
+
+@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2021ApJ...909..209P/abstract')
+def shock_cooling():
+    pass
+
+@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2021ApJ...923L..14M/abstract')
+def _thermal_synchrotron():
+    pass
+
+@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2021ApJ...923L..14M/abstract')
+def thermal_synchrotron_bolometric():
+    pass
 
 @citation_wrapper('https://ui.adsabs.harvard.edu/abs/2021ApJ...923L..14M/abstract')
 def thermal_synchrotron():
     pass
 
-@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2021ApJ...909..209P/abstract')
-def shock_cooling():
+@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2018ApJ...855..103P/abstract')
+def _shocked_cocoon():
+    pass
+
+@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2018ApJ...855..103P/abstract')
+def shocked_cocoon_bolometric():
+    pass
+
+@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2018ApJ...855..103P/abstract')
+def shocked_cocoon():
     pass
 
 @citation_wrapper('https://ui.adsabs.harvard.edu/abs/2022ApJ...928..122M/abstract')
