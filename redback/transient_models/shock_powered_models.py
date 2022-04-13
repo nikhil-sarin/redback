2cc09b1e2549ac1dcd2a8ae95fdf130fa3e63b10
Nikhil Sarin <nikhil.sarin@monash.edu>
2022-04-13 14:51:00 +0200

    _shock cooling function

--- redback/transient_models/shock_powered_models.py
+++ redback/transient_models/shock_powered_models.py
@@ -1,18 +1,39 @@
 import numpy as np
+from collections import namedtuple
 from redback.constants import *
 from redback.utils import calc_kcorrected_properties, citation_wrapper, logger
 
 @citation_wrapper('https://ui.adsabs.harvard.edu/abs/2021ApJ...909..209P/abstract')
-def _shock_cooling():
-    pass
+def _shock_cooling(time, mass, radius, energy, **kwargs):
+    nn = kwargs.get('nn',10)
+    delta = kwargs.get('delta',1.1)
+    kk_pow = (nn - 3) * (3 - delta) / (4 * np.pi * (nn - delta))
+    kappa = 0.2
+    vt = (((nn - 5) * (5 - delta) / ((nn - 3) * (3 - delta))) * (2 * energy / mass))**0.5
+    td = ((3 * kappa * kk_pow * mass) / ((nn - 1) * vt * speed_of_light))**0.5
 
-@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2021ApJ...909..209P/abstract')
-def shock_cooling_bolometric():
-    pass
+    prefactor = np.pi * (nn - 1) / (3 * (nn - 5)) * speed_of_light * radius * vt**2 / kappa
+    lbol_pre_td = prefactor * np.power(td / time, 4 / (nn - 2))
+    lbol_post_td = prefactor * np.exp(-0.5 * (time * time / td / td - 1))
+    lbol = np.zeros(len(time))
+    lbol[time < td] = lbol_pre_td[time < td]
+    lbol[time >= td] = lbol_post_td[time >= td]
 
-@citation_wrapper('https://ui.adsabs.harvard.edu/abs/2021ApJ...909..209P/abstract')
-def shock_cooling():
-    pass
+    tph = np.sqrt(3 * kappa * kk_pow * mass / (2 * (nn - 1) * vt * vt))
+    r_photosphere_pre_td = np.power(tph / time, 2 / (nn - 1)) * vt * time
+    r_photosphere_post_td = (np.power((delta - 1) / (nn - 1) * ((time / td) ** 2 - 1) + 1, -1 / (delta + 1))* vt * time)
+    r_photosphere = np.zeros(len(time))
+    r_photosphere[time < td] = r_photosphere_pre_td[time < td]
+    r_photosphere[time >= td] = r_photosphere_post_td[time >= td]
+
+    sigmaT4 = lbol / (4 * np.pi * r_photosphere**2)
+    temperature = np.power(sigmaT4 / sigma_sb, 0.25)
+
+    output = namedtuple('output', ['lbol', 'r_photosphere', 'temperature'])
+    output.lbol = lbol
+    output.r_photosphere = r_photosphere
+    output.temperature = temperature
+    return output
 
 @citation_wrapper('https://ui.adsabs.harvard.edu/abs/2021ApJ...923L..14M/abstract')
 def _thermal_synchrotron():
