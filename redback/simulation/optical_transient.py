97097f3cb8f75bc63119c357cd6383356e5da2bd
Christian <aerosetzer@gmail.com>
2022-04-13 10:12:17 +0200

    simsurvey integration initial

--- redback/simulation/optical_transient.py
+++ redback/simulation/optical_transient.py
@@ -1,5 +1,15 @@
 import simsurvey
+import sncosmo
 
 
 class Optical_Transient(object):
     def __init__(name=None, parameters=None, observations=None):
+
+        super.__init__()
+
+    def function_wrapper(model, parameters, function=all_models_dict[model]):
+
+        return sncosmo.wrapper
+
+    def simulate_observations():
+        
