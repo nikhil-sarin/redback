cd55093beeafcaf233b3d29288d05c6fb134342e
Christian <aerosetzer@gmail.com>
2022-07-08 19:53:29 +0200

    Updated example with streamlined redback_model_function

--- examples/my_kilonova_example.py
+++ examples/my_kilonova_example.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import redback
 import bilby.core.prior
-from bilby.core.prior import LogUniform, Uniform, Sine
-from bnspopkne.redback_interface import redback_S22_BNS_popkNe as saeev
+from bilby.core.prior import Uniform, Sine, Gaussian
+from bnspopkne.redback_interface import redback_S22_BNS_popkNe_streamlined as saeev
+from bnspopkne import equation_of_state as eos
+from bnspopkne import mappings
 from multiprocessing import Pool, cpu_count
 
 run_pool = Pool(cpu_count() - 1)
@@ -22,22 +24,44 @@ data = redback.get_data.get_kilonova_data_from_open_transient_catalog_data(
 kilonova = redback.kilonova.Kilonova.from_open_access_catalogue(
     name=kne,
     data_mode="flux_density",
-    active_bands=np.array(["u", "g", "r", "i", "z", "y", "J"]),
+    active_bands=np.array(["u", "g", "r", "i", "z", "y"]),
 )
+EOS = "sfho"
+# EOS_path = "/cfs/data/chse9649/input_data/kne_data/eos_data/"
+EOS_path = "/Users/cnsetzer/Documents/Project1_kNe/kne_modeling/eos_data/"
+E1 = eos.get_EOS_table(EOS=EOS, EOS_path=EOS_path)
+tov_mass = eos.get_max_EOS_mass(E1)
 
 # use default priors
 priors = bilby.core.prior.PriorDict()
-priors["m1"] = Uniform(1.0, 2.0, "m1", latex_label=r"$M_1$")
-priors["m2"] = Uniform(1.0, 2.0, "m2", latex_label=r"$M_2$")
+priors["m1"] = Uniform(1.0, tov_mass, "m1", latex_label=r"$M_1$")
+priors["m2"] = Uniform(1.0, tov_mass, "m2", latex_label=r"$M_2$")
 priors["theta_obs"] = Sine(name="theta_obs", latex_label=r"$\iota$")
 priors["redshift"] = Uniform(0.009, 0.011, "redshift", latex_label=r"$z$")
 priors["disk_eff"] = Uniform(0.1, 0.4, "disk_eff", latex_label=r"$\eta_\mathrm{disk}$")
-priors["ra"] = np.deg2rad(197.4503542)
-priors["dec"] = np.deg2rad(-23.3814842)
+priors["peculiar_velocity"] = Gaussian(
+    0.0, 300.0, name="peculiar_velocity", latex_label=r"$v_\mathrm{p}$"
+)
 
+# kappa_grid_path = "/cfs/data/chse9649/input_data/kne_data/opacity_data/Setzer2022_popkNe_opacities.csv"
+kappa_grid_path = "Setzer2022_popkNe_opacities.csv"
+gp_hyperparameter_file = "/cfs/data/chse9649/input_data/kne_data/opacity_data/Setzer2022_popkNe_GP_hyperparam.npy"
+EOS_mass_to_rad = eos.get_radius_from_EOS(E1)
+opacity_data, gp = mappings.construct_opacity_gaussian_process(kappa_grid_path, None)
 
 model_kwargs = dict(
-    frequency=kilonova.filtered_frequencies, output_format="flux_density"
+    frequency=kilonova.filtered_frequencies,
+    output_format="flux_density",
+    sim_gw=False,
+    tov_mass=tov_mass,
+    EOS_name=EOS,
+    EOS_mass_to_rad=EOS_mass_to_rad,
+    opacity_data=opacity_data,
+    grey_opacity_interp=gp,
+    save=False,
+    consistency_check=False,
+    ra=np.deg2rad(197.4503542),
+    dec=np.deg2rad(-23.3814842),
 )
 
 result = redback.fit_model(
