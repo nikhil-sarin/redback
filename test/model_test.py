02bc24d8dee9d549b7a3f56ff1f4053f9d425b56
Nikhil Sarin <nsarin.astro@gmail.com>
2023-03-16 16:16:35 +0100

    Phase model test

--- test/model_test.py
+++ test/model_test.py
@@ -38,3 +38,33 @@ class TestModels(unittest.TestCase):
             function = redback.model_library.all_models_dict[model_name]
             ys = function(times, **prior.sample(), **kwargs)
             self.assertEqual(len(times), len(ys))
+
+class TestPhaseModels(unittest.TestCase):
+    def setUp(self) -> None:
+        self.path_to_files = f"{_dirname}/../redback/priors/"
+        self.prior_files = listdir(self.path_to_files)
+
+    def tearDown(self) -> None:
+        pass
+
+    def get_prior(self, file):
+        prior_dict = bilby.prior.PriorDict()
+        prior_dict.from_file(f"{self.path_to_files}{file}")
+        return prior_dict
+
+    def test_models(self):
+        kwargs = dict(frequency=2e14)
+        times = np.array([1, 2, 3]) + 55855
+        for f in self.prior_files:
+            print(f)
+            model_name = f.replace(".prior", "")
+            if model_name == 'trapped_magnetar':
+                kwargs['output_format'] = 'luminosity'
+            else:
+                kwargs['output_format'] = 'flux_density'
+            prior = self.get_prior(file=f)
+            function = redback.model_library.all_models_dict['t0_base_model']
+            kwargs['base_model'] = model_name
+            kwargs['t0'] = 55855
+            ys = function(times, **prior.sample(), **kwargs)
+            self.assertEqual(len(times), len(ys))
