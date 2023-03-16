5f9ef004f76d7a6d79a77d7cfd6a14e21ae421e6
Nikhil Sarin <nsarin.astro@gmail.com>
2023-03-16 16:42:52 +0100

    test different output formats

--- test/model_test.py
+++ test/model_test.py
@@ -68,3 +68,60 @@ class TestPhaseModels(unittest.TestCase):
             kwargs['t0'] = 55855
             ys = function(times, **prior.sample(), **kwargs)
             self.assertEqual(len(times), len(ys))
+
+class TestMagnitudeOutput(unittest.TestCase):
+    def setUp(self) -> None:
+        self.path_to_files = f"{_dirname}/../redback/priors/"
+        self.prior_files = listdir(self.path_to_files)
+
+    def tearDown(self) -> None:
+        pass
+
+    def get_prior(self, file):
+        prior_dict = bilby.prior.PriorDict()
+        prior_dict.from_file(f"{self.path_to_files}{file}")
+        return prior_dict
+
+    def test_models(self):
+        kwargs = dict(frequency=2e14, bands='ztfg')
+        times = np.array([1, 2, 3])
+        for f in self.prior_files:
+            print(f)
+            model_name = f.replace(".prior", "")
+            if model_name == 'trapped_magnetar':
+                kwargs['output_format'] = 'luminosity'
+            else:
+                kwargs['output_format'] = 'magnitude'
+            prior = self.get_prior(file=f)
+            function = redback.model_library.all_models_dict[model_name]
+            ys = function(times, **prior.sample(), **kwargs)
+            self.assertEqual(len(times), len(ys))
+
+class TestFluxOutput(unittest.TestCase):
+    def setUp(self) -> None:
+        self.path_to_files = f"{_dirname}/../redback/priors/"
+        self.prior_files = listdir(self.path_to_files)
+
+    def tearDown(self) -> None:
+        pass
+
+    def get_prior(self, file):
+        prior_dict = bilby.prior.PriorDict()
+        prior_dict.from_file(f"{self.path_to_files}{file}")
+        return prior_dict
+
+    def test_models(self):
+        kwargs = dict(frequency=2e14, bands='ztfg')
+        times = np.array([1, 2, 3])
+        valid_models = ['arnett', 'one_component_kilonova_model', 'slsn', 'tde_analytical', 'basic_mergernova']
+        for f in self.prior_files:
+            model_name = f.replace(".prior", "")
+            if model_name in valid_models:
+                print(f)
+                kwargs['output_format'] = 'flux'
+                prior = self.get_prior(file=f)
+                function = redback.model_library.all_models_dict[model_name]
+                ys = function(times, **prior.sample(), **kwargs)
+            else:
+                ys = np.ones(len(times))
+            self.assertEqual(len(times), len(ys))
