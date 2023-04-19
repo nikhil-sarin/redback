8ffed670b26c7535301710671263305e17c7e136
Nikhil Sarin <nsarin.astro@gmail.com>
2023-04-19 16:17:55 +0200

    Bugfix extinction

--- redback/transient_models/extinction_models.py
+++ redback/transient_models/extinction_models.py
@@ -100,6 +100,7 @@ def _evaluate_extinction_model(time, av, model_type, **kwargs):
         kwargs['base_model'] = kwargs.get('submodel', 'arnett_bolometric')
     if kwargs['output_format'] == 'flux_density':
         frequency = kwargs['frequency']
+        angstroms = redback.utils.nu_to_lambda(frequency)
         if isinstance(frequency, float):
             frequency = np.ones(len(time)) * frequency
         temp_kwargs = kwargs.copy()
@@ -107,7 +108,7 @@ def _evaluate_extinction_model(time, av, model_type, **kwargs):
         function = _get_correct_function(base_model=base_model, model_type=model_type)
         flux_density = function(time, **temp_kwargs)
         r_v = kwargs.get('r_v', 3.1)
-        flux_density = _perform_extinction(flux_density=flux_density, angstroms=frequency, av=av, r_v=r_v)
+        flux_density = _perform_extinction(flux_density=flux_density, angstroms=angstroms, av=av, r_v=r_v)
         return flux_density
     else:
         temp_kwargs = kwargs.copy()
@@ -120,7 +121,7 @@ def _evaluate_extinction_model(time, av, model_type, **kwargs):
         time_observer_frame = spectra_tuple.time
         r_v = kwargs.get('r_v', 3.1)
         flux_density = _perform_extinction(flux_density=flux_density, angstroms=lambdas, av=av, r_v=r_v)
-        return sed.get_correct_output_format_from_spectra(time=time_obs, time_eval=time_observer_frame/day_to_s,
+        return sed.get_correct_output_format_from_spectra(time=time_obs, time_eval=time_observer_frame,
                                                               spectra=flux_density, lambda_array=spectra_tuple.lambdas,
                                                               **kwargs)
 
