29a10a3a4482bd672eaa25054130dca79a04eabf
Wendy <115488486+wfw23@users.noreply.github.com>
2023-08-02 11:26:55 +0200

    reverting own test code in extinction_models

--- redback/transient_models/extinction_models.py
+++ redback/transient_models/extinction_models.py
@@ -83,9 +83,6 @@ def _perform_extinction(flux_density, angstroms, av, r_v):
     if isinstance(angstroms, float):
         angstroms = np.array([angstroms])
     mag_extinction = extinction.fitzpatrick99(angstroms, av, r_v=r_v)
-    #print(mag_extinction)
-    #if mag_extinction > 1000:
-     #   mag_extinction=0
     flux_density = extinction.apply(mag_extinction, flux_density)
     return flux_density
 
