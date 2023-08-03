17477d7863fe1577d7baee662c512f2153a3de4b
Wendy <115488486+wfw23@users.noreply.github.com>
2023-08-03 12:06:02 +0200

    fixed extinction over estimating and added generic combined model

--- redback/transient_models/extinction_models.py
+++ redback/transient_models/extinction_models.py
@@ -80,9 +80,13 @@ def _perform_extinction(flux_density, angstroms, av, r_v):
     :return: flux
     """
     import extinction  # noqa
+    import numpy.ma as ma
     if isinstance(angstroms, float):
-        angstroms = np.array([angstroms])
+        angstroms = np.array([angstroms])    
     mag_extinction = extinction.fitzpatrick99(angstroms, av, r_v=r_v)
+    for i in range(len(mag_extinction)):
+        if mag_extinction[i]>20:
+            mag_extinction[i]=0
     flux_density = extinction.apply(mag_extinction, flux_density)
     return flux_density
 
