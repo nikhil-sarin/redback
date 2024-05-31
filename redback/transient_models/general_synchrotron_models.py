4808a5052e3f08da51085889f1bcb023faca168f
comand <conoromand>
2024-05-31 15:33:42 +0100

    removed ipdb

--- redback/transient_models/general_synchrotron_models.py
+++ redback/transient_models/general_synchrotron_models.py
@@ -7,8 +7,6 @@ from redback.constants import day_to_s, solar_mass, km_cgs, au_cgs, speed_of_lig
 from scipy import integrate
 from scipy.interpolate import interp1d
 
-import ipdb
-
 def _calc_free_free_abs(frequency, Y_fe, Zbar, mej, radius_2darray, F_nu_2darray):
     """
     :param frequency: frequency to calculate
