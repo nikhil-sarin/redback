c574468e2ddd93cbc25b0ec746f62d27aef21fb0
Nikhil Sarin <nsarin.astro@gmail.com>
2025-02-19 22:39:58 +1100

    v1.03 in init

--- redback/__init__.py
+++ redback/__init__.py
@@ -4,5 +4,5 @@ from redback.transient import afterglow, kilonova, prompt, supernova, tde
 from redback.sampler import fit_model
 from redback.utils import setup_logger
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 setup_logger(log_level='info')
