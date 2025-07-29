41fccd8f987c0028a96986cf85d3ec246a2c1f13
Nikhil Sarin <nsarin.astro@gmail.com>
2025-07-29 23:40:03 +0200

    Add smooth exponential and powerlaw model

--- redback/transient_models/phenomenological_models.py
+++ redback/transient_models/phenomenological_models.py
@@ -3,6 +3,33 @@ from redback.utils import citation_wrapper
 from redback.constants import speed_of_light_si
 
 
+def smooth_exponential_powerlaw(time, a_1, tpeak, alpha_1, alpha_2, smoothing_factor, **kwargs):
+    """
+    Smoothed version of exponential power law
+
+    :param time: time array in seconds
+    :param a_1: exponential amplitude scale
+    :param alpha_1: first exponent
+    :param alpha_2: second exponent
+    :param tpeak: peak time in seconds
+    :param smoothing_factor: controls transition smoothness (higher = smoother)
+    :param kwargs: Additional parameters
+    :return: In whatever units set by a_1
+    """
+    t_norm = time / tpeak
+
+    # Smooth transition function using tanh or similar
+    transition = 0.5 * (1 + np.tanh(smoothing_factor * np.log(t_norm)))
+
+    # Pre-peak behavior
+    pre_peak = a_1 * (t_norm ** alpha_1)
+
+    # Post-peak behavior
+    post_peak = a_1 * (t_norm ** alpha_2)
+
+    # Smooth combination
+    return pre_peak * (1 - transition) + post_peak * transition
+
 def exp_rise_powerlaw_decline(t, t0, m_peak, tau_rise, alpha, t_peak, **kwargs):
     """
     Compute a smooth light-curve model (in magnitudes) with an exponential rise
