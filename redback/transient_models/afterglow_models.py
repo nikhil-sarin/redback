Refactor: Fix anti-patterns and extract frequency normalization helpers
Co-authored-by: nikhil-sarin <42257246+nikhil-sarin@users.noreply.github.com>

redback/transient_models/afterglow_models.py
@@ -1,6 +1,7 @@
 from astropy.cosmology import Planck18 as cosmo  # noqa
 from inspect import isfunction
-from redback.utils import logger, citation_wrapper, calc_ABmag_from_flux_density, lambda_to_nu, bands_to_frequency
+from redback.utils import (logger, citation_wrapper, calc_ABmag_from_flux_density, lambda_to_nu, bands_to_frequency,
+                           normalize_frequency_to_time_array)
 from redback.constants import day_to_s, speed_of_light, solar_mass, proton_mass, electron_mass, sigma_T
 from redback.sed import get_correct_output_format_from_spectra
 import astropy.units as uu
@@ -725,9 +726,7 @@ def tophat_redback(time, redshift, thv, loge0, thc, logn0, p, logepse, logepsb, 
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)
@@ -789,9 +788,7 @@ def gaussian_redback(time, redshift, thv, loge0, thc, thj, logn0, p, logepse, lo
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)
@@ -856,9 +853,7 @@ def twocomponent_redback(time, redshift, thv, loge0, thc, thj, logn0, p, logepse
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)
@@ -925,9 +920,7 @@ def powerlaw_redback(time, redshift, thv, loge0, thc, thj, logn0, p, logepse, lo
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)
@@ -993,9 +986,7 @@ def alternativepowerlaw_redback(time, redshift, thv, loge0, thc, thj, logn0, p, 
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)
@@ -1061,9 +1052,7 @@ def doublegaussian_redback(time, redshift, thv, loge0, thc, thj, logn0, p, logep
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)
@@ -1130,9 +1119,7 @@ def tophat_redback_refreshed(time, redshift, thv, loge0, thc, g1, et, s1,
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)
@@ -1200,9 +1187,7 @@ def gaussian_redback_refreshed(time, redshift, thv, loge0, thc, thj, g1, et, s1,
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)
@@ -1273,9 +1258,7 @@ def twocomponent_redback_refreshed(time, redshift, thv, loge0, thc, thj, g1, et,
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)
@@ -1347,9 +1330,7 @@ def powerlaw_redback_refreshed(time, redshift, thv, loge0, thc, thj, g1, et, s1,
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)
@@ -1421,9 +1402,7 @@ def alternativepowerlaw_redback_refreshed(time, redshift, thv, loge0, thc, thj, 
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)
@@ -1495,9 +1474,7 @@ def doublegaussian_redback_refreshed(time, redshift, thv, loge0, thc, thj, g1, e
     :return: flux density or AB mag. Note this is going to give the monochromatic magnitude at the effective frequency for the band.
         For a proper calculation of the magntitude use the sed variant models.
     """
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     k = kwargs.get('k', 0)
     a1 = kwargs.get('a1', 1)
     exp = kwargs.get('expansion', 1)

redback/transient_models/extinction_models.py
@@ -2,7 +2,7 @@ from inspect import isfunction
 import numpy as np
 import redback.utils
 from redback.transient_models.fireball_models import predeceleration
-from redback.utils import logger, calc_ABmag_from_flux_density, citation_wrapper, lambda_to_nu
+from redback.utils import logger, calc_ABmag_from_flux_density, citation_wrapper, lambda_to_nu, normalize_frequency_to_array
 import astropy.units as uu
 import redback.sed as sed
 from redback.constants import day_to_s
@@ -221,9 +221,7 @@ def _evaluate_extinction_model(time, av_host, av_mw=0.0, model_type=None, **kwar
     mw_law = kwargs.pop('mw_law', 'fitzpatrick99')
 
     if kwargs['output_format'] == 'flux_density':
-        frequency = kwargs['frequency']
-        if isinstance(frequency, float):
-            frequency = np.ones(len(time)) * frequency
+        frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
 
         angstroms = redback.utils.nu_to_lambda(frequency)
 

redback/transient_models/general_synchrotron_models.py
@@ -4,7 +4,7 @@ from redback.transient_models.magnetar_driven_ejecta_models import _ejecta_dynam
 from redback.transient_models.shock_powered_models import _emissivity_pl, _emissivity_thermal, _tau_nu, _c_j, _c_alpha, _g_theta, _low_freq_apl_correction, _low_freq_jpl_correction
 from redback.transient_models.afterglow_models import _get_kn_dynamics, _pnu_synchrotron
 from astropy.cosmology import Planck18 as cosmo
-from redback.utils import calc_kcorrected_properties, citation_wrapper, logger, get_csm_properties, nu_to_lambda, lambda_to_nu, velocity_from_lorentz_factor, calc_ABmag_from_flux_density
+from redback.utils import calc_kcorrected_properties, citation_wrapper, logger, get_csm_properties, nu_to_lambda, lambda_to_nu, velocity_from_lorentz_factor, calc_ABmag_from_flux_density, normalize_frequency_to_time_array
 from redback.constants import day_to_s, solar_mass, km_cgs, au_cgs, speed_of_light, qe, electron_mass, proton_mass, sigma_T
 from scipy import integrate
 from scipy.interpolate import interp1d
@@ -248,9 +248,7 @@ def kilonova_afterglow_redback(time, redshift, loge0, mej, logn0, logepse, logep
     mu = 1.0
     blueshift = Gamma * (1.0 - beta * mu)
 
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     fnu_func = {}
     for nu in frequency:
         Fnu_opt_thin = _pnu_synchrotron(nu * blueshift * (1.0 + redshift), B, gamma_m, gamma_c, Ne, p) * (1.0 + redshift) / (
@@ -316,9 +314,7 @@ def kilonova_afterglow_nakarpiran(time, redshift, loge0, mej, logn0, logepse, lo
     fnu_dec_dict = {}
     fnu_func = {}
     temp_time = np.linspace(0.1, 100, 200) * t_dec
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     for freq in frequency:
         # Eq. 11 in Nakar & Piran 2011 (in Mjy)
         fnu_dec_dict[freq] = 0.3 * (Eej / 1e49) * n0 ** (0.25 * (p + 1)) * (epsilon_B / 1e-1) ** (0.25 * (p + 1)) * (
@@ -536,9 +532,7 @@ def synchrotron_massloss(time, redshift, v_s, log_Mdot_vwind, logepsb, logepse, 
     """
     cosmology = kwargs.get('cosmology', cosmo)
     dl = cosmology.luminosity_distance(redshift).cgs.value
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     frequency, time = calc_kcorrected_properties(frequency=frequency, redshift=redshift, time=time)   
 
     eps_e = 10.0 ** logepse
@@ -588,9 +582,7 @@ def synchrotron_ism(time, redshift, v_s, logn0, logepsb, logepse, p, **kwargs):
     """
     cosmology = kwargs.get('cosmology', cosmo)
     dl = cosmology.luminosity_distance(redshift).cgs.value
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     frequency, time = calc_kcorrected_properties(frequency=frequency, redshift=redshift, time=time)
 
     n_ism = 10.0 ** logn0
@@ -640,9 +632,7 @@ def synchrotron_pldensity(time, redshift, v_s, logA, s, logepsb, logepse, p, **k
     """
     cosmology = kwargs.get('cosmology', cosmo)
     dl = cosmology.luminosity_distance(redshift).cgs.value
-    frequency = kwargs['frequency']
-    if isinstance(frequency, float):
-        frequency = np.ones(len(time)) * frequency
+    frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
     frequency, time = calc_kcorrected_properties(frequency=frequency, redshift=redshift, time=time)
 
     A = 10.0 ** logA

redback/transient_models/supernova_models.py
@@ -9,7 +9,8 @@ import redback.sed as sed
 import redback.photosphere as photosphere
 from astropy.cosmology import Planck18 as cosmo  # noqa
 from redback.utils import (calc_kcorrected_properties, citation_wrapper, logger, get_csm_properties, nu_to_lambda,
-                           lambda_to_nu, velocity_from_lorentz_factor, build_spectral_feature_list)
+                           lambda_to_nu, velocity_from_lorentz_factor, build_spectral_feature_list,
+                           normalize_frequency_to_array)
 from redback.constants import day_to_s, solar_mass, km_cgs, au_cgs, speed_of_light, sigma_sb
 from inspect import isfunction
 import astropy.units as uu
@@ -56,7 +57,7 @@ def sncosmo_models(time, redshift, model_kwargs=None, **kwargs):
     model.set(z=redshift)
     model.set(t0=peak_time)
 
-    if model_kwargs == None:
+    if model_kwargs is None:
         _model_kwargs = {}
         for x in kwargs['model_kwarg_names']:
             _model_kwargs[x] = kwargs[x]
@@ -81,8 +82,7 @@ def sncosmo_models(time, redshift, model_kwargs=None, **kwargs):
     if kwargs['output_format'] == 'flux_density':
         frequency = kwargs['frequency']
 
-        if isinstance(frequency, float):
-            frequency = np.array([frequency])
+        frequency = normalize_frequency_to_array(frequency)
 
         if (len(frequency) != 1 or len(frequency) == len(time)):
             raise ValueError('frequency array must be of length 1 or same size as time array')

redback/transient_models/tde_models.py
@@ -3,7 +3,7 @@ import redback.interaction_processes as ip
 import redback.sed as sed
 import redback.photosphere as photosphere
 from redback.utils import calc_kcorrected_properties, citation_wrapper, calc_tfb, lambda_to_nu, \
-    calc_ABmag_from_flux_density, calc_flux_density_from_ABmag, bands_to_frequency
+    calc_ABmag_from_flux_density, calc_flux_density_from_ABmag, bands_to_frequency, normalize_frequency_to_array
 import redback.constants as cc
 import redback.transient_models.phenomenological_models as pm
 
@@ -266,9 +266,7 @@ def cooling_envelope(time, redshift, mbh_6, stellar_mass, eta, alpha, beta, **kw
     time_obs = time
 
     if kwargs['output_format'] == 'flux_density':
-        frequency = kwargs['frequency']
-        if isinstance(frequency, float):
-            frequency = np.ones(len(time)) * frequency
+        frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
 
         # convert to source frame time and frequency
         time = time * cc.day_to_s
@@ -471,9 +469,7 @@ def gaussianrise_cooling_envelope(time, redshift, peak_time, sigma_t, mbh_6, ste
     f1 = pm.gaussian_rise(time=stitching_point, a_1=1., peak_time=peak_time * cc.day_to_s, sigma_t=sigma_t * cc.day_to_s)
 
     if kwargs['output_format'] == 'flux_density':
-        frequency = kwargs['frequency']
-        if isinstance(frequency, float):
-            frequency = np.ones(len(time)) * frequency
+        frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
 
         # convert to source frame time and frequency
         frequency, time = calc_kcorrected_properties(frequency=frequency, redshift=redshift, time=time)
@@ -592,9 +588,7 @@ def bpl_cooling_envelope(time, redshift, peak_time, alpha_1, alpha_2, mbh_6, ste
                                  alpha_1=alpha_1, alpha_2=alpha_2)
 
     if kwargs['output_format'] == 'flux_density':
-        frequency = kwargs['frequency']
-        if isinstance(frequency, float):
-            frequency = np.ones(len(time)) * frequency
+        frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)
 
         # convert to source frame time and frequency
         frequency, time = calc_kcorrected_properties(frequency=frequency, redshift=redshift, time=time)
@@ -1576,9 +1570,7 @@ def stream_stream_tde(time, redshift, mbh_6, mstar, c1, f, h_r, inc_tcool, del_o
     r_eff = np.sqrt(full_lbol / (np.pi * cc.sigma_sb * full_temp**4.0))            
         
     if kwargs['output_format'] == 'flux_density':
-        frequency = kwargs['frequency']
-        if isinstance(frequency, float):
-            frequency = np.ones(len(time)) * frequency           
+        frequency = normalize_frequency_to_time_array(kwargs['frequency'], time)           
     
         # convert to source frame time and frequency
         frequency, time = calc_kcorrected_properties(frequency=frequency, redshift=redshift, time=time)

redback/utils.py
@@ -40,6 +40,34 @@ def find_nearest(array, value):
     return array[idx], idx
 
 
+def normalize_frequency_to_array(frequency):
+    """
+    Normalize frequency input to a numpy array.
+    
+    :param frequency: Frequency value(s) - can be float or array-like
+    :return: Frequency as numpy array
+    """
+    if isinstance(frequency, float):
+        frequency = np.array([frequency])
+    return np.asarray(frequency)
+
+
+def normalize_frequency_to_time_array(frequency, time):
+    """
+    Normalize frequency to match time array length.
+    
+    If frequency is a scalar, returns an array of that frequency value
+    matching the length of the time array.
+    
+    :param frequency: Frequency value(s) - can be float or array-like
+    :param time: Time array to match length with
+    :return: Frequency array matching time array length
+    """
+    if isinstance(frequency, float):
+        frequency = np.ones(len(time)) * frequency
+    return np.asarray(frequency)
+
+
 def download_pointing_tables():
     """
     Download the pointing tables from zenodo.
@@ -86,7 +114,7 @@ def check_kwargs_validity(kwargs):
     :param kwargs:
     :return:
     """
-    if kwargs == None:
+    if kwargs is None:
         logger.info("No kwargs passed to function")
         return kwargs
     if 'output_format' not in kwargs.keys():