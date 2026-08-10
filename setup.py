0d44025b8363b91ab6437eb057d149e0ea5fd242
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2026-08-09 19:42:46 -0700

    Format `package_data` list in setup.py

--- setup.py
+++ setup.py
@@ -22,8 +22,16 @@ setup(
         'redback.transient_models.afterglow_models',
     ],
     package_dir={'redback': 'redback', },
-    package_data={'redback': ['priors/*', 'priors/non_default_priors/*', 'tables/*', 'tables/xsect/*',
-                              'tables/guillochon_tde_data/*/*', 'plot_styles/*']},
+    package_data={
+        'redback': [
+            'priors/*',
+            'priors/non_default_priors/*',
+            'tables/*',
+            'tables/xsect/*',
+            'tables/guillochon_tde_data/*/*',
+            'plot_styles/*'
+            ]
+        },
     install_requires=[
         "numpy",
         "numba",
